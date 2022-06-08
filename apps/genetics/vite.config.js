b3fd3cb084043c2cd911ef4a14711434c4a1dd9a
carcruz <me@carcruzcast.com>
2022-06-08 15:59:40 +0100

    fix: fix travis

--- apps/genetics/vite.config.js
+++ apps/genetics/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import svgrPlugin from 'vite-plugin-svgr';
-import react from '@vitejs/plugin-react';
-import graphql from '@rollup/plugin-graphql';
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
+import graphql from "@rollup/plugin-graphql";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../../dist-genetics',
+    outDir: "../../bundle-genetics",
   },
   plugins: [
     graphql(),
