5eb04b942257b79cf903bf24b1a866aa7bb34140
Luca Fumis <lucaf@ebi.ac.uk>
2022-07-11 15:48:06 +0100

    Temporarily update API to production for preview purposes

--- apps/genetics/src/config.js
+++ apps/genetics/src/config.js
@@ -1,7 +1,9 @@
 const config = {
   apiUrl:
     window.configApiUrl ??
-    'https://open-targets-genetics-dev.ew.r.appspot.com/graphql',
+    // 'https://open-targets-genetics-dev.ew.r.appspot.com/graphql',
+    // TODO: production API to be used for testing/preview purposes only; revert before merging
+    'https://api.genetics.opentargets.org/graphql',
   googleTagManagerID: window.configGoogleTagManagerID ?? null,
   helpdeskEmail: window.configHelpdeskEmail ?? 'helpdesk@opentargets.org',
   profile: window.configProfile ?? {},
