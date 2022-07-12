4cb4786ba7817785b2e8735b7af4da939c6b185d
Carlos Cruz <me@carcruzcast.com>
2022-07-12 12:04:31 +0100

    feat: point to production API

--- apps/genetics/src/config.js
+++ apps/genetics/src/config.js
@@ -1,7 +1,5 @@
 const config = {
-  apiUrl:
-    window.configApiUrl ??
-    'https://open-targets-genetics-dev.ew.r.appspot.com/graphql',
+  apiUrl: window.configApiUrl ?? 'https://api.genetics.opentargets.org/graphql',
   googleTagManagerID: window.configGoogleTagManagerID ?? null,
   helpdeskEmail: window.configHelpdeskEmail ?? 'helpdesk@opentargets.org',
   profile: window.configProfile ?? {},
