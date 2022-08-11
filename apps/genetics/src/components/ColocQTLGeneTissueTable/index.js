2cb3417671fbbd0aeb95bd668abfb11fd6077f5c
remo87 <remo87@gmail.com>
2022-08-11 08:42:03 +0100

    refactor ColocQTLGeneTissueTable

---
+++ apps/genetics/src/components/ColocQTLGeneTissueTable/index.js
@@ -0,0 +1,7 @@
+import ColocQTLGeneTissueTableCmp from "./ColocQTLGeneTissueTable";
+
+export { getTableColumns } from "./ColumnRenderers";
+
+export { useColocTable } from "./useColocTable";
+
+export default ColocQTLGeneTissueTableCmp;
