3684b4e4e04fa200c81bf3afcd81ef475848e0b2
remo87 <remo87@gmail.com>
2022-08-15 14:48:56 +0100

    format variant

--- apps/genetics/src/utils/variant.js
+++ apps/genetics/src/utils/variant.js
@@ -6,8 +6,8 @@ export function variantGetInfo(data) {
 }
 
 export function variantTransformAssociatedIndexVariants(data) {
-  const associationsFlattened = data.indexVariantsAndStudiesForTagVariant.associations.map(
-    (d) => {
+  const associationsFlattened =
+    data.indexVariantsAndStudiesForTagVariant.associations.map((d) => {
       const { indexVariant, study, ...rest } = d;
       return {
         indexVariantId: indexVariant.id,
@@ -19,14 +19,13 @@ export function variantTransformAssociatedIndexVariants(data) {
         pubAuthor: study.pubAuthor,
         ...rest,
       };
-    }
-  );
+    });
   return associationsFlattened;
 }
 
 export function variantTransformAssociatedTagVariants(data) {
-  const associationsFlattened = data.tagVariantsAndStudiesForIndexVariant.associations.map(
-    (d) => {
+  const associationsFlattened =
+    data.tagVariantsAndStudiesForIndexVariant.associations.map((d) => {
       const { tagVariant, study, ...rest } = d;
       return {
         tagVariantId: tagVariant.id,
@@ -38,25 +37,24 @@ export function variantTransformAssociatedTagVariants(data) {
         pubAuthor: study.pubAuthor,
         ...rest,
       };
-    }
-  );
+    });
   return associationsFlattened;
 }
 
 export const variantPopulations = [
-  { code: "AFR", description: "African/African-American" },
-  { code: "AMR", description: "Latino/Admixed American" },
-  { code: "ASJ", description: "Ashkenazi Jewish" },
-  { code: "EAS", description: "East Asian" },
-  { code: "FIN", description: "Finnish" },
-  { code: "NFE", description: "Non-Finnish European" },
-  { code: "NFEEST", description: "Non-Finnish European Estonian" },
+  { code: 'AFR', description: 'African/African-American' },
+  { code: 'AMR', description: 'Latino/Admixed American' },
+  { code: 'ASJ', description: 'Ashkenazi Jewish' },
+  { code: 'EAS', description: 'East Asian' },
+  { code: 'FIN', description: 'Finnish' },
+  { code: 'NFE', description: 'Non-Finnish European' },
+  { code: 'NFEEST', description: 'Non-Finnish European Estonian' },
   {
-    code: "NFENWE",
-    description: "Non-Finnish European North-Western European",
+    code: 'NFENWE',
+    description: 'Non-Finnish European North-Western European',
   },
-  { code: "NFESEU", description: "Non-Finnish European Southern European" },
-  { code: "OTH", description: "Other (population not assigned)" },
+  { code: 'NFESEU', description: 'Non-Finnish European Southern European' },
+  { code: 'OTH', description: 'Other (population not assigned)' },
 ];
 
 export function variantParseGenesForVariantSchema(data) {
@@ -64,7 +62,7 @@ export function variantParseGenesForVariantSchema(data) {
   const currentQtls = genesForVariantSchema.qtls;
   if (currentQtls.length === 0) return genesForVariantSchema;
   //possibly add new fields
-  if (currentQtls[0].id === "pqtl") {
+  if (currentQtls[0].id === 'pqtl') {
     const pqtl = currentQtls[0];
     const restQtls = currentQtls.slice(1, pqtl.length);
     const sourceDescriptionBreakdown = parseSourceDescriptionBreakdown(
@@ -72,21 +70,18 @@ export function variantParseGenesForVariantSchema(data) {
     );
     const sourceLabel = parseSourceLabel(pqtl.sourceLabel);
     const newPqtl = { ...pqtl, sourceDescriptionBreakdown, sourceLabel };
-    const newQtls = [
-      newPqtl,
-      ...restQtls,
-    ];    
+    const newQtls = [newPqtl, ...restQtls];
     return { ...genesForVariantSchema, qtls: newQtls };
   }
   return genesForVariantSchema;
 }
 
-function parseSourceDescriptionBreakdown(description = "") {
+function parseSourceDescriptionBreakdown(description = '') {
   if (!description) return;
-  return description.replace(" Sun *et al.* (2018)", "");
+  return description.replace(' Sun *et al.* (2018)', '');
 }
 
-function parseSourceLabel(label = "") {
+function parseSourceLabel(label = '') {
   if (!label) return;
-  return label.replace(" (Sun, 2018)", "");
+  return label.replace(' (Sun, 2018)', '');
 }
