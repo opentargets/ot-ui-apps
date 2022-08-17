8530e0d036c76ae3d2e0f2d3d0fbce7faeee2ea8
Chintan Mehta <mchintan24@gmail.com>
2022-08-17 16:52:40 +0100

    Refactored Gene page to functional component

--- apps/genetics/src/pages/GenePage/GenePage.jsx
+++ apps/genetics/src/pages/GenePage/GenePage.jsx
@@ -49,9 +49,11 @@ const styles = theme => {
   };
 };
 
-class GenePage extends React.Component {
-  handleColocTraitFilter = newColocTraitFilterValue => {
-    const { colocTraitFilter, ...rest } = this._parseQueryProps();
+function GenePage({ history, match }) {
+  const { geneId } = match.params;
+
+  const handleColocTraitFilter = newColocTraitFilterValue => {
+    const { colocTraitFilter, ...rest } = _parseQueryProps();
     const newQueryParams = {
       ...rest,
     };
@@ -60,30 +62,29 @@ class GenePage extends React.Component {
         d => d.value
       );
     }
-    this._stringifyQueryProps(newQueryParams);
+    _stringifyQueryProps(newQueryParams);
   };
-  handleTraitFilter = newTraitFilterValue => {
-    const { traitFilter, ...rest } = this._parseQueryProps();
+  const handleTraitFilter = newTraitFilterValue => {
+    const { traitFilter, ...rest } = _parseQueryProps();
     const newQueryParams = {
       ...rest,
     };
     if (newTraitFilterValue && newTraitFilterValue.length > 0) {
       newQueryParams.traitFilter = newTraitFilterValue.map(d => d.value);
     }
-    this._stringifyQueryProps(newQueryParams);
+    _stringifyQueryProps(newQueryParams);
   };
-  handleAuthorFilter = newFilterValue => {
-    const { authorFilter, ...rest } = this._parseQueryProps();
+  const handleAuthorFilter = newFilterValue => {
+    const { authorFilter, ...rest } = _parseQueryProps();
     const newQueryParams = {
       ...rest,
     };
     if (newFilterValue && newFilterValue.length > 0) {
       newQueryParams.authorFilter = newFilterValue.map(d => d.value);
     }
-    this._stringifyQueryProps(newQueryParams);
+    _stringifyQueryProps(newQueryParams);
   };
-  _parseQueryProps() {
-    const { history } = this.props;
+  const _parseQueryProps = () => {
     const queryProps = queryString.parse(history.location.search);
 
     // single values need to be put in lists
@@ -103,40 +104,37 @@ class GenePage extends React.Component {
         : [queryProps.authorFilter];
     }
     return queryProps;
-  }
-  _stringifyQueryProps(newQueryParams) {
-    const { history } = this.props;
+  };
+  const _stringifyQueryProps = newQueryParams => {
     history.replace({
       ...history.location,
       search: queryString.stringify(newQueryParams),
     });
-  }
-  render() {
-    const { match } = this.props;
-    const { geneId } = match.params;
-    const {
-      colocTraitFilter: colocTraitFilterUrl,
-      traitFilter: traitFilterUrl,
-      authorFilter: authorFilterUrl,
-    } = this._parseQueryProps();
-    return (
-      <BasePage>
-        <Header geneId={geneId} />
-        <L2Gpipeline
-          geneId={geneId}
-          traitFilterUrl={traitFilterUrl}
-          authorFilterUrl={authorFilterUrl}
-          handleTraitFilter={this.handleTraitFilter}
-          handleAuthorFilter={this.handleAuthorFilter}
-        />
-        <ColocAnalysis
-          geneId={geneId}
-          handleColocTraitFilter={this.handleColocTraitFilter}
-          colocTraitFilterUrl={colocTraitFilterUrl}
-        />
-      </BasePage>
-    );
-  }
+  };
+
+  const {
+    colocTraitFilter: colocTraitFilterUrl,
+    traitFilter: traitFilterUrl,
+    authorFilter: authorFilterUrl,
+  } = _parseQueryProps();
+
+  return (
+    <BasePage>
+      <Header geneId={geneId} />
+      <L2Gpipeline
+        geneId={geneId}
+        traitFilterUrl={traitFilterUrl}
+        authorFilterUrl={authorFilterUrl}
+        handleTraitFilter={handleTraitFilter}
+        handleAuthorFilter={handleAuthorFilter}
+      />
+      <ColocAnalysis
+        geneId={geneId}
+        handleColocTraitFilter={handleColocTraitFilter}
+        colocTraitFilterUrl={colocTraitFilterUrl}
+      />
+    </BasePage>
+  );
 }
 
 export default withStyles(styles)(GenePage);
