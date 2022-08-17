23ad4185ef13a11abf47153601ebc469335c3650
Chintan Mehta <mchintan24@gmail.com>
2022-08-17 16:35:24 +0100

    L2G pipeline component added

--- apps/genetics/src/pages/GenePage/GenePage.jsx
+++ apps/genetics/src/pages/GenePage/GenePage.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import queryString from 'query-string';
 import _ from 'lodash';
 import { withStyles } from '@material-ui/core/styles';
-import { Query } from '@apollo/client/react/components';
-
-import { SectionHeading } from '../../ot-ui-components';
 
 import BasePage from '../BasePage';
-import AssociatedStudiesTable from '../../components/AssociatedStudiesTable';
 import Header from './Header';
 
-import GENE_PAGE_QUERY from '../../queries/GenePageQuery.gql';
 import ColocAnalysis from '../../sections/gene/ColocAnalysis/ColocAnalysis';
+import L2Gpipeline from '../../sections/gene/L2Gpipeline/L2Gpipeline';
 
-function hasGeneData(data) {
-  return data && data.geneInfo;
-}
-
-function geneData(data) {
-  return data.geneInfo;
-}
-
-function hasAssociatedStudies(data) {
-  return data && data.studiesAndLeadVariantsForGeneByL2G;
-}
-
-const styles = (theme) => {
+const styles = theme => {
   return {
     section: {
       height: '100%',
@@ -67,35 +50,35 @@ const styles = (theme) => {
 };
 
 class GenePage extends React.Component {
-  handleColocTraitFilter = (newColocTraitFilterValue) => {
+  handleColocTraitFilter = newColocTraitFilterValue => {
     const { colocTraitFilter, ...rest } = this._parseQueryProps();
     const newQueryParams = {
       ...rest,
     };
     if (newColocTraitFilterValue && newColocTraitFilterValue.length > 0) {
       newQueryParams.colocTraitFilter = newColocTraitFilterValue.map(
-        (d) => d.value
+        d => d.value
       );
     }
     this._stringifyQueryProps(newQueryParams);
   };
-  handleTraitFilter = (newTraitFilterValue) => {
+  handleTraitFilter = newTraitFilterValue => {
     const { traitFilter, ...rest } = this._parseQueryProps();
     const newQueryParams = {
       ...rest,
     };
     if (newTraitFilterValue && newTraitFilterValue.length > 0) {
-      newQueryParams.traitFilter = newTraitFilterValue.map((d) => d.value);
+      newQueryParams.traitFilter = newTraitFilterValue.map(d => d.value);
     }
     this._stringifyQueryProps(newQueryParams);
   };
-  handleAuthorFilter = (newFilterValue) => {
+  handleAuthorFilter = newFilterValue => {
     const { authorFilter, ...rest } = this._parseQueryProps();
     const newQueryParams = {
       ...rest,
     };
     if (newFilterValue && newFilterValue.length > 0) {
-      newQueryParams.authorFilter = newFilterValue.map((d) => d.value);
+      newQueryParams.authorFilter = newFilterValue.map(d => d.value);
     }
     this._stringifyQueryProps(newQueryParams);
   };
@@ -139,101 +122,18 @@ class GenePage extends React.Component {
     return (
       <BasePage>
         <Header geneId={geneId} />
-        <Query query={GENE_PAGE_QUERY} variables={{ geneId }}>
-          {({ loading, error, data }) => {
-            const isValidGene = hasGeneData(data, geneId);
-            const gene = isValidGene ? geneData(data) : {};
-
-            // all
-            const associatedStudies =
-              isValidGene && hasAssociatedStudies(data)
-                ? data.studiesAndLeadVariantsForGeneByL2G
-                : [];
-
-            // filtered
-            const associatedStudiesFiltered = associatedStudies.filter((d) => {
-              return (
-                (traitFilterUrl
-                  ? traitFilterUrl.indexOf(d.study.traitReported) >= 0
-                  : true) &&
-                (authorFilterUrl
-                  ? authorFilterUrl.indexOf(d.study.pubAuthor) >= 0
-                  : true)
-              );
-            });
-
-            const traitFilterOptions = _.sortBy(
-              _.uniq(
-                associatedStudiesFiltered.map((d) => d.study.traitReported)
-              ).map((d) => ({
-                label: d,
-                value: d,
-                selected: traitFilterUrl
-                  ? traitFilterUrl.indexOf(d) >= 0
-                  : false,
-              })),
-              [(d) => !d.selected, 'value']
-            );
-            const traitFilterValue = traitFilterOptions.filter(
-              (d) => d.selected
-            );
-            const authorFilterOptions = _.sortBy(
-              _.uniq(
-                associatedStudiesFiltered.map((d) => d.study.pubAuthor)
-              ).map((d) => ({
-                label: d,
-                value: d,
-                selected: authorFilterUrl
-                  ? authorFilterUrl.indexOf(d) >= 0
-                  : false,
-              })),
-              [(d) => !d.selected, 'value']
-            );
-            const authorFilterValue = authorFilterOptions.filter(
-              (d) => d.selected
-            );
-
-            const { chromosome, start, end, symbol } = gene;
-            return (
-              <React.Fragment>
-                <Helmet>
-                  <title>{symbol}</title>
-                </Helmet>
-                <SectionHeading
-                  heading="Associated studies: locus-to-gene pipeline"
-                  subheading={`Which studies are associated with ${symbol}?`}
-                  entities={[
-                    {
-                      type: 'study',
-                      fixed: false,
-                    },
-                    {
-                      type: 'gene',
-                      fixed: true,
-                    },
-                  ]}
-                />
-                <AssociatedStudiesTable
-                  loading={loading}
-                  error={error}
-                  data={associatedStudiesFiltered}
-                  geneId={geneId}
-                  geneSymbol={symbol}
-                  chromosome={chromosome}
-                  position={Math.round((start + end) / 2)}
-                  traitFilterValue={traitFilterValue}
-                  traitFilterOptions={traitFilterOptions}
-                  traitFilterHandler={this.handleTraitFilter}
-                  authorFilterValue={authorFilterValue}
-                  authorFilterOptions={authorFilterOptions}
-                  authorFilterHandler={this.handleAuthorFilter}
-                  filenameStem={`${geneId}-associated-studies`}
-                />
-                <ColocAnalysis geneId={geneId} handleColocTraitFilter={this.handleColocTraitFilter} colocTraitFilterUrl={colocTraitFilterUrl}/>
-              </React.Fragment>
-            );
-          }}
-        </Query>
+        <L2Gpipeline
+          geneId={geneId}
+          traitFilterUrl={traitFilterUrl}
+          authorFilterUrl={authorFilterUrl}
+          handleTraitFilter={this.handleTraitFilter}
+          handleAuthorFilter={this.handleAuthorFilter}
+        />
+        <ColocAnalysis
+          geneId={geneId}
+          handleColocTraitFilter={this.handleColocTraitFilter}
+          colocTraitFilterUrl={colocTraitFilterUrl}
+        />
       </BasePage>
     );
   }
