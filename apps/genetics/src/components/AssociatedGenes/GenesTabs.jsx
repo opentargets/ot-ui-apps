6450894eacfc16b13ca0dd0f233c15afdfe5c441
remo87 <remo87@gmail.com>
2022-08-17 14:16:53 +0100

    update tab color to identify status

--- apps/genetics/src/components/AssociatedGenes/GenesTabs.jsx
+++ apps/genetics/src/components/AssociatedGenes/GenesTabs.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Tabs, Tab } from '../../ot-ui-components';
 import { OVERVIEW, isDisabledColumn } from './utils';
+import theme from '../theme';
 
 export const GenesTabs = ({ schemas, value, dataAll, handleChange }) => {
   return (
     <Tabs variant="scrollable" value={value} onChange={handleChange}>
       <Tab label="Summary" value={OVERVIEW} />
-      {schemas.map((schema) => {
+      {schemas.map(schema => {
         return (
           <Tab
             key={schema.sourceId}
             value={schema.sourceId}
             label={schema.sourceLabel}
+            style={{ color: theme.grey[900] }}
             disabled={isDisabledColumn(dataAll, schema.sourceId)}
           />
         );
