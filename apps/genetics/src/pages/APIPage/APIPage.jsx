42f0da0c35515a7ab45d5ad027698d2e1678dfde
Chintan Mehta <mchintan24@gmail.com>
2022-08-24 16:17:46 +0100

    sample queries added

--- apps/genetics/src/pages/APIPage/APIPage.jsx
+++ apps/genetics/src/pages/APIPage/APIPage.jsx
@@ -3,36 +3,36 @@ import { React, useState, Suspense, lazy } from 'react';
 import { Grid, makeStyles } from '@material-ui/core';
 import 'graphiql/graphiql.min.css';
 import APIHeader from '../../sections/api-page/Header/APIHeader';
+import SampleQueries from '../../sections/api-page/SampleQueries';
+
 
 // lazy load GraphiQL and remove Logo and Toolbar
 const GraphiQL = lazy(() =>
-  import('graphiql').then(module => {
+import('graphiql').then(module => {
     module.default.Logo = () => null;
     module.default.Toolbar = () => null;
     return module;
-  })
+})
 );
 
 const useStyles = makeStyles({
-  container: {
-    minHeight: '600px',
-  },
-  buttonMargin: {
-    marginBottom: '12px',
-  },
+    container: {
+        minHeight: '600px',
+    }
 });
 
+import config from '../../config';
 // TODO: move to util file
 async function fetcher(graphQLParams) {
-  // const data = await fetch(config.urlApi, {
-  //   method: 'POST',
-  //   headers: {
-  //     Accept: 'application/json',
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(graphQLParams),
-  // });
-  // return data.json();
+  const data = await fetch(config.apiUrl, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(graphQLParams),
+  });
+  return data.json();
 }
 
 function APIPage() {
@@ -44,7 +44,7 @@ function APIPage() {
       <APIHeader />
       <Grid className={classes.container} container spacing={3}>
         <Grid item md={3} xl={2}>
-          sample query accordians here
+          <SampleQueries setQuery={setQuery}/>
         </Grid>
         <Grid item md={9} xl={10}>
           <Suspense fallback={<div>Loading...</div>}>
--- apps/genetics/src/sections/api-page/SampleQueries/SampleQueries.jsx
+++ apps/genetics/src/sections/api-page/SampleQueries/SampleQueries.jsx
@@ -1,9 +1,150 @@
-import React from 'react'
+import React from 'react';
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Button,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-function SampleQueries() {
+import TARGET_ASSOCS from './TargetAssocs.gql';
+import DISEASE_ASSOCS from './DiseaseAssocs.gql';
+import TARGET_DISEASE_EVIDENCE from './TargetDiseaseEvidence.gql';
+import TARGET_ANNOTATION from './TargetAnnotation.gql';
+import DISEASE_ANNOTATION from './DiseaseAnnotation.gql';
+import DRUG_ANNOTATION from './DrugAnnotation.gql';
+
+const useStyles = makeStyles({
+  buttonMargin: {
+    marginBottom: '12px',
+  },
+});
+
+function SampleQueries({ setQuery }) {
+  const classes = useStyles();
   return (
-    <div>SampleQueries</div>
-  )
+    <>
+      <Typography variant="h5" paragraph>
+        Example queries
+      </Typography>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="subtitle2">
+            Target-disease association
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>
+            <Typography variant="subtitle2" display="block" paragraph>
+              Find targets associated with a specific disease or phenotype
+            </Typography>
+            <Button
+              className={classes.buttonMargin}
+              variant="contained"
+              color="primary"
+              onClick={() => setQuery(DISEASE_ASSOCS.loc.source.body)}
+            >
+              Run sample query
+            </Button>
+            <Typography variant="subtitle2" display="block" paragraph>
+              Find diseases and phenotypes associated with a specific target
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setQuery(TARGET_ASSOCS.loc.source.body)}
+            >
+              Run sample query
+            </Button>
+          </div>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="subtitle2">Target-disease evidence</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>
+            <Typography variant="subtitle2" display="block" paragraph>
+              Explore evidence that supports a specific target-disease
+              association
+            </Typography>
+            <Button
+              className={classes.buttonMargin}
+              variant="contained"
+              color="primary"
+              onClick={() => setQuery(TARGET_DISEASE_EVIDENCE.loc.source.body)}
+            >
+              Run sample query
+            </Button>
+          </div>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="subtitle2">Target annotation</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>
+            <Typography variant="subtitle2" display="block" paragraph>
+              Find tractability and safety information for a specific target
+            </Typography>
+            <Button
+              className={classes.buttonMargin}
+              variant="contained"
+              color="primary"
+              onClick={() => setQuery(TARGET_ANNOTATION.loc.source.body)}
+            >
+              Run sample query
+            </Button>
+          </div>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="subtitle2">Disease annotation</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>
+            <Typography variant="subtitle2" display="block" paragraph>
+              Find clinical signs and symptoms for a specific disease
+            </Typography>
+            <Button
+              className={classes.buttonMargin}
+              variant="contained"
+              color="primary"
+              onClick={() => setQuery(DISEASE_ANNOTATION.loc.source.body)}
+            >
+              Run sample query
+            </Button>
+          </div>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="subtitle2">Drug annotation</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>
+            <Typography variant="subtitle2" display="block" paragraph>
+              Find approval status and withdrawn and black-box warning for a
+              specific drug
+            </Typography>
+            <Button
+              className={classes.buttonMargin}
+              variant="contained"
+              color="primary"
+              onClick={() => setQuery(DRUG_ANNOTATION.loc.source.body)}
+            >
+              Run sample query
+            </Button>
+          </div>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  );
 }
 
-export default SampleQueries
+export default SampleQueries;
