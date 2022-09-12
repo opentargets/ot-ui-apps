4030db9522aad76fe51e8bdaf3b68839ba536c3f
Luca Fumis <lucaf@ebi.ac.uk>
2022-09-12 23:55:19 +0100

    Update evidence widget id for cancer gene census

--- apps/platform/src/sections/evidence/CancerGeneCensus/index.js
+++ apps/platform/src/sections/evidence/CancerGeneCensus/index.js
@@ -1,5 +1,6 @@
+const id = 'cancer_gene_census';
 export const definition = {
-  id: 'cancerGeneCensus',
+  id: id,
   name: 'Cancer Gene Census',
   shortName: 'CC',
   hasData: ({ cancerGeneCensusSummary }) => cancerGeneCensusSummary.count > 0,
