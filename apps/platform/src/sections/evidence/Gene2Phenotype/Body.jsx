a7acadf6c274129aab096a026bceb957d6ca9969
Luca Fumis <lucaf@ebi.ac.uk>
2022-11-01 18:40:32 +0000

    Update literature column to PublicationDrawer component

--- apps/platform/src/sections/evidence/Gene2Phenotype/Body.jsx
+++ apps/platform/src/sections/evidence/Gene2Phenotype/Body.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { List, ListItem, Typography } from '@material-ui/core';
 import { useQuery } from '@apollo/client';
 
-import { DataTable, TableDrawer } from '../../../components/Table';
+import { DataTable } from '../../../components/Table';
 import { defaultRowsPerPageOptions, naLabel } from '../../../constants';
 import Description from './Description';
 import { epmcUrl } from '../../../utils/urls';
@@ -11,6 +11,7 @@ import Link from '../../../components/Link';
 import SectionItem from '../../../components/Section/SectionItem';
 import { sentenceCase } from '../../../utils/global';
 import Tooltip from '../../../components/Tooltip';
+import { PublicationsDrawer } from '../../../components/PublicationsDrawer';
 import OPEN_TARGETS_GENETICS_QUERY from './sectionQuery.gql';
 
 const g2pUrl = (studyId, symbol) =>
@@ -97,22 +98,14 @@ const columns = [
   },
   {
     id: 'literature',
+    label: 'Literature',
     renderCell: ({ literature }) => {
-      const literatureList =
-        literature?.reduce((acc, id) => {
-          if (id === 'NA') return acc;
-
-          return [
-            ...acc,
-            {
-              name: id,
-              url: epmcUrl(id),
-              group: 'literature',
-            },
-          ];
-        }, []) || [];
-
-      return <TableDrawer entries={literatureList} />;
+      const entries = literature
+        ? literature.map(id => {
+            return { name: id, url: epmcUrl(id), group: 'literature' };
+          })
+        : [];
+      return <PublicationsDrawer entries={entries} />;
     },
   },
 ];
