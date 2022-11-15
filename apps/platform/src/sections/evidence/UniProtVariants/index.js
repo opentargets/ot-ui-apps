007fc882a142d495883ec7fba0a7ad0ee933674a
Luca Fumis <lucaf@ebi.ac.uk>
2022-11-15 23:49:51 +0000

    Include summary query in definition

--- apps/platform/src/sections/evidence/UniProtVariants/index.js
+++ apps/platform/src/sections/evidence/UniProtVariants/index.js
@@ -1,4 +1,5 @@
 import { isPrivateEvidenceSection } from '../../../utils/partnerPreviewUtils';
+import UNIPROT_VARIANTS_SUMMARY from './UniprotVariantsSummaryQuery.gql';
 
 const id = 'uniprot_variants';
 export const definition = {
@@ -7,6 +8,7 @@ export const definition = {
   shortName: 'UV',
   hasData: data => data.uniprotVariantsSummary.count > 0,
   isPrivate: isPrivateEvidenceSection(id),
+  countQuery: UNIPROT_VARIANTS_SUMMARY,
 };
 
 export { default as Summary } from './Summary';
