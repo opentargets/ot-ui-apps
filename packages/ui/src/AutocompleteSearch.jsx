b3409454a08b0b9ab9d6f36f3205e3e13ac85036
Chintan Mehta <mchintan24@gmail.com>
2022-12-07 11:16:03 +0000

    modal and dropdown height fixed

--- packages/ui/src/AutocompleteSearch.jsx
+++ packages/ui/src/AutocompleteSearch.jsx
@@ -24,7 +24,7 @@ const theme = createTheme({
         boxShadow: "none",
       },
       listbox: {
-        maxHeight: "40% !important",
+        maxHeight: "40vh !important",
       },
       option: {
         margin: "0 1rem",
