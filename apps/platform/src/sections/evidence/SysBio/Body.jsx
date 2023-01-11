e753085b401fd3fc80f071fa335b20980fbba7ae
Luca Fumis <lucaf@ebi.ac.uk>
2023-01-11 23:36:06 +0000

    Update gene signatures (sysbio) widget to bodycore

--- apps/platform/src/sections/evidence/SysBio/Body.jsx
+++ apps/platform/src/sections/evidence/SysBio/Body.jsx
@@ -59,8 +59,26 @@ const columns = [
   },
 ];
 
-function Body({ definition, id: { ensgId, efoId }, label: { symbol, name } }) {
-  const variables = { ensemblId: ensgId, efoId };
+export function Body({ definition, id, label }) {
+  const { data: summaryData } = usePlatformApi(
+    Summary.fragments.SysBioSummaryFragment
+  );
+  const count = summaryData.sysBio.count;
+  
+  if(!count || count < 1) {
+    return null
+  }
+
+  return <BodyCore definition={definition} id={id} label={label} count={count} />
+}
+
+export function BodyCore({ definition, id, label, count }) {
+  const { ensgId: ensemblId, efoId } = id;
+  const variables = {
+    ensemblId,
+    efoId,
+    size: count,
+  };
 
   const request = useQuery(SYSBIO_QUERY, {
     variables,
@@ -71,7 +89,7 @@ function Body({ definition, id: { ensgId, efoId }, label: { symbol, name } }) {
       definition={definition}
       chipText={dataTypesMap.affected_pathway}
       request={request}
-      renderDescription={() => <Description symbol={symbol} name={name} />}
+      renderDescription={() => <Description symbol={label.symbol} name={label.name} />}
       renderBody={data => (
         <DataTable
           columns={columns}
