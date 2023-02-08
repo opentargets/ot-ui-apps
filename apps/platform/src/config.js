257b3cccfe76929d29b6599ff1f71d19f38cd3b7
carcruz <me@carcruzcast.com>
2023-02-08 10:15:58 +0000

    fix: reset to default config

--- apps/platform/src/config.js
+++ apps/platform/src/config.js
@@ -1,9 +1,11 @@
+// Configuration Object
 const config = {
   urlApi:
     window.configUrlApi ??
-    'https://api.partner-platform.dev.opentargets.xyz/api/v4/graphql',
+    'https://api.platform.dev.opentargets.xyz/api/v4/graphql',
   googleTagManagerID: window.configGoogleTagManagerID ?? null,
   efoURL: window.configEFOURL ?? '/data/ontology/efo_json/diseases_efo.jsonl',
+
   profile: window.configProfile ?? {},
   downloadsURL: window.configDownloadsURL ?? '/data/downloads.json',
   geneticsPortalUrl:
