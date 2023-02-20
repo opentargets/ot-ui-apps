7c8261ae9f5c40acd6e4645c8fe60b1f6d53204a
carcruz <me@carcruzcast.com>
2023-02-20 01:27:28 +0000

    fix: target view switch export

--- apps/platform/src/components/AssociationsToolkit/index.js
+++ apps/platform/src/components/AssociationsToolkit/index.js
@@ -1,6 +1,6 @@
 export { default as AdvanceOptionsMenu } from './AdvanceOptionsMenu';
 export { default as TableAssociations } from './association/TableAssociations';
-export { default as TargetPrioritisationSwitch } from './association/TargetPrioritisationSwitch';
+export { default as TargetPrioritisationSwitch } from './TargetPrioritisationSwitch';
 export {
   default as AssociationsContext,
   AssociationsProvider,
