5f6fae211886a2679e004d25061a2f7987688ec2
Chintan Mehta <mchintan24@gmail.com>
2023-03-10 16:40:17 +0000

    project table ppp header and icon changes

--- apps/platform/src/pages/ProjectsPage/ProjectsPage.jsx
+++ apps/platform/src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,42 +1,48 @@
 import React, { Fragment } from 'react';
-import { Paper, Box, Typography } from '@material-ui/core';
+import { Paper, Box, Typography, makeStyles } from '@material-ui/core';
 import projectsData from './projects-data.json';
 import { DataTable } from '../../components/Table';
 import Link from '../../components/Link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCheck,
-  faXmark,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { faCircleDot, faCircle  } from '@fortawesome/free-regular-svg-icons';
 
-const columns = [
-  {
-    id: 'otar_code',
-    label: 'Project Code',
-    renderCell: ({ otar_code }) => {
-      return otar_code ? (
-        <Link to={`http://home.opentargets.org/${otar_code}`} external newTab>
-          {otar_code}
-        </Link>
-      ) : null;
-    },
+const useStyles = makeStyles(theme => ({
+  icon: {
+    color: theme.palette.primary.main,
   },
-  { id: 'project_name', label: 'Project Name' },
-  { id: 'project_lead', label: 'Project Lead' },
-  { id: 'generates_data', label: 'Generates Data' },
-  {
-    id: 'integrates_in_PPP',
-    label: 'Integrates into PPP',
-    renderCell: ({ integrates_in_PPP }) => {
-      const icon = integrates_in_PPP === 'Y' ? faCheck : faXmark;
-      return <FontAwesomeIcon size="lg" icon={icon} />;
-    },
-  },
-  { id: 'project_status', label: 'Project Status' },
-  { id: 'open_targets_therapeutic_area', label: 'Therapeutic Area' },
-];
+}));
 
 function ProjectPage() {
+  const classes = useStyles();
+  const columns = [
+    {
+      id: 'otar_code',
+      label: 'Project Code',
+      renderCell: ({ otar_code }) => {
+        return otar_code ? (
+          <Link to={`http://home.opentargets.org/${otar_code}`} external newTab>
+            {otar_code}
+          </Link>
+        ) : null;
+      },
+    },
+    { id: 'project_name', label: 'Project Name' },
+    { id: 'project_lead', label: 'Project Lead' },
+    { id: 'generates_data', label: 'Generates Data' },
+    {
+      id: 'integrates_in_PPP',
+      label: 'Currently integrates into PPP',
+      renderCell: ({ integrates_in_PPP }) => {
+        const icon = integrates_in_PPP === 'Y' ? faCircleCheck : faCircle;
+        return (
+          <FontAwesomeIcon size="lg" icon={icon} className={classes.icon} />
+        );
+      },
+    },
+    { id: 'project_status', label: 'Project Status' },
+    { id: 'open_targets_therapeutic_area', label: 'Therapeutic Area' },
+  ];
   return (
     <Fragment>
       <Typography variant="h4" component="h1" paragraph>
@@ -59,7 +65,11 @@ function ProjectPage() {
       </Typography>
       <Typography paragraph>
         PPP specific documentation can be found{' '}
-        <Link to="https://platform-docs.opentargets.org/partner-preview-platform" external newTab>
+        <Link
+          to="http://home.opentargets.org/ppp-documentation"
+          external
+          newTab
+        >
           here
         </Link>
       </Typography>
