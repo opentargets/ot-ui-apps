0c02ee1cbd72465811c5e3d6de9da72c2d977cae
carcruz <me@carcruzcast.com>
2023-04-05 18:47:47 +0100

    feat: env configuration

--- apps/platform/src/config.js
+++ apps/platform/src/config.js
@@ -1,12 +1,9 @@
 // Configuration Object
 const config = {
-  urlApi:
-    window.configUrlApi ??
-    'https://api.platform.dev.opentargets.xyz/api/v4/graphql',
+  urlApi: window.configUrlApi ?? import.meta.env.VITE_API_URL,
+  profile: window.configProfile ?? {},
   googleTagManagerID: window.configGoogleTagManagerID ?? null,
   efoURL: window.configEFOURL ?? '/data/ontology/efo_json/diseases_efo.jsonl',
-
-  profile: window.configProfile ?? {},
   downloadsURL: window.configDownloadsURL ?? '/data/downloads.json',
   geneticsPortalUrl:
     window.configGeneticsPortalUrl ?? 'https://genetics.opentargets.org',
