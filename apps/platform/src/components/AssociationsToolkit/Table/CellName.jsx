5945ea70a91b8ac0be6adf88781013ce8f239133
carcruz <me@carcruzcast.com>
2023-04-21 20:08:10 +0100

    feat: add name on cell name tooltip

--- apps/platform/src/components/AssociationsToolkit/Table/CellName.jsx
+++ apps/platform/src/components/AssociationsToolkit/Table/CellName.jsx
@@ -1,5 +1,10 @@
-import { styled } from '@material-ui/styles';
 import { useState } from 'react';
+import { styled } from '@material-ui/styles';
+import { Typography } from '@material-ui/core';
+import { faDna } from '@fortawesome/free-solid-svg-icons';
+import { faStethoscope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import Tooltip from '../AotFTooltip';
 import Link from '../../Link';
 import useAotfContext from '../hooks/useAotfContext';
@@ -40,13 +45,16 @@ const LinksTooltipContent = styled('span')({
   gap: '5px',
 });
 
-function TooltipContent({ id, entity }) {
+function TooltipContent({ id, entity, name, icon }) {
   const profileURL = `/${entity}/${id}`;
   const associationsURL = `/${entity}/${id}/associations`;
   return (
     <LinksTooltipContent>
-      <Link to={profileURL}>Profile</Link>
-      <Link to={associationsURL}>Associations</Link>
+      <Typography variant="subtitle1">
+        <FontAwesomeIcon icon={icon} /> {name}
+      </Typography>
+      <Link to={profileURL}>Go to Profile</Link>
+      <Link to={associationsURL}>Go to Associations</Link>
     </LinksTooltipContent>
   );
 }
@@ -56,13 +64,21 @@ function CellName({ name, rowId }) {
   const { entityToGet } = useAotfContext();
 
   const rowEntity = entityToGet === 'target' ? 'target' : 'disease';
+  const icon = rowEntity === 'target' ? faDna : faStethoscope;
 
   return (
     <NameContainer>
       <Tooltip
         open={open}
         onClose={() => setOpen(false)}
-        content={<TooltipContent name={name} entity={rowEntity} id={rowId} />}
+        content={
+          <TooltipContent
+            name={name}
+            entity={rowEntity}
+            id={rowId}
+            icon={icon}
+          />
+        }
       >
         <TextContainer onClick={() => setOpen(true)}>
           <Name>{name}</Name>
