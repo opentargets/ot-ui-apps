fd240c23056362afb309eeefac40c4ce7de70e9d
Luca Fumis <lucaf@ebi.ac.uk>
2023-05-25 16:43:21 +0100

    Update description

--- apps/platform/src/sections/target/DepMap/Description.jsx
+++ apps/platform/src/sections/target/DepMap/Description.jsx
@@ -5,9 +5,9 @@ import Link from '../../../components/Link';
 function Description({ symbol }) {
   return (
     <>
-      Cancer DepMap info for <strong>{symbol}</strong> -- TODO: description --. Source:{' '}
-      <Link external to="#">
-        -- TODO: SOURCE --
+      Gene Essentiality assessment obtained through CRISPR loss-of-function screens in a wide range of cancer cell lines. Source:{' '}
+      <Link external to="https://depmap.org/portal/">
+        DepMap Portal
       </Link>
       .
     </>
