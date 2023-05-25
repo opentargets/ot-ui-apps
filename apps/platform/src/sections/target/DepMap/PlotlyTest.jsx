1bc35c5c27b07890a67cca85d541bb2f2fbf9f50
Luca Fumis <lucaf@ebi.ac.uk>
2023-05-25 16:43:59 +0100

    Add config options

--- apps/platform/src/sections/target/DepMap/PlotlyTest.jsx
+++ apps/platform/src/sections/target/DepMap/PlotlyTest.jsx
@@ -19,42 +19,62 @@ class PlotlyTest extends Component {
 
         const depMapEssentiality = data.map(d => ({
             x: d.screens.map(s => s.geneEffect),
+            // x0: -1,
             hovertext: d.screens.map(s => `id:${s.diseaseCellLineId} exp:${s.expression}`),
             type: 'box',
             name: d.tissueName,
+            mode: 'markers',
             // jitter: 0.3,
             // pointpos: -1.8,
             marker: {
               color: '#3589CA',
-              size: d.screens.map(s => (s.expression+1)*20),
+              size: d.screens.map(s => (s.expression+1)*2), //not working for boxplot
               opacity: 0.5,
               showscale: true,
             },
-            boxpoints: 'all'
+            boxpoints: 'all',
+            // line: {
+            //     color: '#F00',
+            // },
+            legendgrouptitle: {
+                text: 'legend title',
+            },
+            showlegend: true,
+            // legendwidth: 900, // not working?
+
         }));
 
         return (
             <Plot
             data = {depMapEssentiality}
-            // data={[
-                // trace1,
-            // {
-            //     x: [1, 2, 3],
-            //     y: [2, 6, 3],
-            //     type: 'scatter',
-            //     mode: 'lines+markers',
-            //     marker: {color: 'red'},
-            // },
-            // {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-            // ]}
             layout={ {
-                width: 1000, 
-                height: 1400, 
-                title: 'depMapEssentiality',
-                facet_col: [],
+                width: 1000, //window.innerWidth, 
+                height: (28*50), 
+                title: 'DepMapEssentiality',
                 // annotations: data.map(d => ({
                 //     text: 'alse',
                 // })),
+                autosize: true,
+                yaxis:{
+                    automargin: 'width',
+                },
+                shapes: [{
+                    // draw the reference line at -1
+                    'type': 'line',
+                    'x0': -1,
+                    'y0': -.5,
+                    'x1': -1,
+                    'y1': depMapEssentiality.length-.5,
+                    'line': {
+                      'color': '#f00',
+                      'width': 1,
+                    //   'dash': 'dashdot'
+                    }
+                  }],
+                legend: {
+                    bgcolor: '#0f0',
+                    entrywidth: 300,
+                }
             } }
         />
         );
