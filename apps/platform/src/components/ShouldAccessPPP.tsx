431ed40e2b0a7d3aa8637baa0a1d1416ce6ac2f7
Chintan Mehta <mchintan24@gmail.com>
2023-05-31 10:29:18 +0100

    should access ppp popup

---
+++ apps/platform/src/components/ShouldAccessPPP.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+  makeStyles,
+} from '@material-ui/core';
+import { useLocation } from 'react-router-dom';
+
+const PPP_API_URL =
+  'https://api.partner-platform.opentargets.org/api/v4/graphql';
+const PPP_WEB_URL = 'https://partner-platform.opentargets.org';
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    padding: '1em 1em 2em',
+    borderRadius: '12px',
+  },
+}));
+
+function ShouldAccessPPP() {
+  const location = useLocation();
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const classes = useStyles();
+
+  useEffect(() => {
+    checkPPPaccess();
+  }, []);
+
+  const checkPPPaccess = () => {
+    fetch(PPP_API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        operationName: 'DataVersion',
+        variables: {},
+        query: `query DataVersion {
+        meta {
+          dataVersion {
+            month
+            year
+            __typename
+          }
+          __typename
+	
+      }
+    }`,
+      }),
+    })
+      .then(response => {
+        if (response.status === 200) handleOpen();
+      })
+      .catch(() => {
+        console.log('Does not have access to Partner Preview Platform');
+      });
+  };
+
+  const handleOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleClose = () => {
+    setDialogOpen(false);
+  };
+
+  const goToPPP = () => {
+    window.location.href = `${PPP_WEB_URL}${location.pathname}`;
+  };
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      aria-labelledby="should-access-ppp"
+      open={dialogOpen}
+      classes={{
+        paper: classes.paper,
+      }}
+    >
+      <DialogTitle>Looks like you are a Partner!</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          We are pleased to inform you that you have access to our exclusive
+          Partner Preview Platform. This will have early access to
+          the latest features, updates, and innovations before they are made
+          available to the public.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} variant="outlined">
+          Continue to Public Version
+        </Button>
+        <Button onClick={goToPPP} variant="contained" color="primary">
+          Continue on Partner Preview Platform
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default ShouldAccessPPP;
