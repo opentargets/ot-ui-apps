8c5fc5225125929a28f9ee5027718d86d85f8313
Luca Fumis <lucaf@ebi.ac.uk>
2023-06-20 11:33:00 +0100

    feat: plug API data to depmap widget

--- apps/platform/src/sections/target/DepMap/Body.jsx
+++ apps/platform/src/sections/target/DepMap/Body.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useQuery } from '@apollo/client';
 import Description from './Description';
 import SectionItem from '../../../components/Section/SectionItem';
@@ -6,14 +5,10 @@ import DepmapPlot from './DepmapPlot';
 
 import DEPMAP_QUERY from './Depmap.gql';
 
-// TODO: temporary sample data
-import data0 from './data/data.json';
 
 function Section({ definition, id, label: symbol }) {
   const variables = { ensemblId: id };
   const request = useQuery(DEPMAP_QUERY, { variables });
-
-  const { depMapEssentiality } = data0;
 
   return (
     <SectionItem
@@ -21,10 +16,9 @@ function Section({ definition, id, label: symbol }) {
       request={request}
       renderDescription={() => <Description symbol={symbol} />}
       renderBody={data => {
-        // TODO: depMapEssentiality will come from data
         return (
           <>
-            <DepmapPlot data={depMapEssentiality} />
+            <DepmapPlot data={data.target.depMapEssentiality} />
           </>
         );
       }}
