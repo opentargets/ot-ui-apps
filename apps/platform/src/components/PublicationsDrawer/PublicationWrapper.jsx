22bc78f8c59ed44e179efea2920538aa5801a6f2
Luca Fumis <luca.fumis@gmail.com>
2023-07-24 09:03:33 +0100

    fix: incorrect imports

--- apps/platform/src/components/PublicationsDrawer/PublicationWrapper.jsx
+++ apps/platform/src/components/PublicationsDrawer/PublicationWrapper.jsx
@@ -5,7 +5,7 @@ import {
   faFileAlt,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Box, Button, makeStyles, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 import { LongText } from 'ui';
