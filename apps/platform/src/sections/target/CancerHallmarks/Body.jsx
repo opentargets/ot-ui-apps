64ebc89cc5ed93cbe1ea891525ebe4359d14f991
Luca Fumis <luca.fumis@gmail.com>
2023-07-27 00:11:29 +0100

    feat: update MUI for section target/cancerhallmarks

--- apps/platform/src/sections/target/CancerHallmarks/Body.jsx
+++ apps/platform/src/sections/target/CancerHallmarks/Body.jsx
@@ -1,4 +1,5 @@
-import { Box, makeStyles, Typography } from '@material-ui/core';
+import { Box, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { useQuery } from '@apollo/client';
 
 import ChipList from '../../../components/ChipList';
