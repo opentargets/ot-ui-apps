7c2a7e808afc5e46b6f73a695be27c75001f2af9
Carlos Cruz <me@carcruzcast.com>
2023-08-18 03:05:29 +0100

    fix: render disease assoc sections

--- apps/platform/src/components/AssociationsToolkit/Table/SectionRender.jsx
+++ apps/platform/src/components/AssociationsToolkit/Table/SectionRender.jsx
@@ -1,12 +1,18 @@
 import { Suspense } from 'react';
 import { styled } from '@material-ui/styles';
 import { LoadingBackdrop } from 'ui';
+import { ENTITIES } from '../utils';
 
 import targetSections from '../../../sections/targetSections';
 import evidenceSections from '../../../sections/evidenceSections';
 
 const LoadingContainer = styled('div')({
   margin: '50px 0',
+});
+
+const Container = styled('div')({
+  marginTop: '10px',
+  marginBottom: '40px',
 });
 
 function LoadingSection() {
@@ -17,11 +23,6 @@ function LoadingSection() {
     </LoadingContainer>
   );
 }
-
-const Container = styled('div')({
-  marginTop: '10px',
-  marginBottom: '40px',
-});
 
 function SectionNotFound() {
   return <div>Section not found</div>;
@@ -53,7 +54,7 @@ export function SectionRender({
     case 'prioritisations': {
       Component = targetSections.get(section);
       const { targetSymbol } = row.original;
-      ensgId = entity === 'disease' ? rowId : id;
+      ensgId = entity === ENTITIES.DISEASE ? rowId : id;
       label = targetSymbol;
       componentId = ensgId;
       entityOfSection = 'target';
@@ -62,10 +63,11 @@ export function SectionRender({
     case 'associations': {
       Component = evidenceSections.get(section);
       const { diseaseName, targetSymbol } = row.original;
-      ensgId = entity === 'disease' ? rowId : id;
-      efoId = entity === 'disease' ? id : rowId;
+      ensgId = entity === ENTITIES.DISEASE ? rowId : id;
+      efoId = entity === ENTITIES.DISEASE ? id : rowId;
       componentId = { ensgId, efoId };
       label = { symbol: targetSymbol, name: diseaseName };
+      entityOfSection = 'disease';
       break;
     }
     default:
