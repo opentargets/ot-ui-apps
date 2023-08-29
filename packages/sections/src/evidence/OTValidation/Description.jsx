c19b86882b9646f39309c3469a9cb96f67f5e320
Chintan Mehta <mchintan24@gmail.com>
2023-08-29 17:11:57 +0100

    description link update

--- packages/sections/src/evidence/OTValidation/Description.jsx
+++ packages/sections/src/evidence/OTValidation/Description.jsx
@@ -7,7 +7,7 @@ function Description({ symbol, name }) {
       and have undergone independent prioritisation and orthogonal experimental
       validation in the Open Targets Validation Lab (OTVL), associating{' '}
       <strong>{symbol}</strong> and <strong>{name}</strong>. Source:{' '}
-      <Link external to="http://home.opentargets.org/OTAR2059">
+      <Link external to="http://home.opentargets.org/ppp-documentation-otar2059">
         Open Targets Validation Lab
       </Link>
     </>
