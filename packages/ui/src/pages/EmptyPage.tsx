8e74c267fe372483bc9adf200604d4f15dc42bdd
Chintan Mehta <22376522+chinmehta@users.noreply.github.com>
2023-09-04 16:37:31 +0100

    [Platform]: 404 links updated (#185)
    * links updated
    
    * url added from config
    
    * genetics doc link updated

--- packages/ui/src/pages/EmptyPage.tsx
+++ packages/ui/src/pages/EmptyPage.tsx
@@ -50,10 +50,12 @@ const useStyles = makeStyles((theme) => ({
 type EmptyPageProps = {
   children: ReactNode;
   documentationLink?: string;
+  communityLink?: string;
 };
 function EmptyPage({
   children,
   documentationLink = "https://platform-docs.opentargets.org",
+  communityLink = "https://community.opentargets.org",
 }: EmptyPageProps) {
   const classes = useStyles();
 
@@ -73,8 +75,12 @@ function EmptyPage({
           <div>{children}</div>
           <Typography>
             You deserve a fresh start. Maybe our
-            <Link to={documentationLink}> Documentation</Link> or{" "}
-            <Link to={"https://community.opentargets.org/"}>
+            <Link external to={documentationLink}>
+              {" "}
+              Documentation
+            </Link>{" "}
+            or{" "}
+            <Link external to={communityLink}>
               Community page
             </Link>{" "}
             can help!
