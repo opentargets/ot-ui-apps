4ecbbc02e5e3072df6299238aa7fba4243834228
Chintan Mehta <22376522+chinmehta@users.noreply.github.com>
2023-09-08 11:26:05 +0100

    [Platform]: style borders (#207)
    * section description font size
    
    * card borders added

--- packages/ui/src/components/Section/SectionItem.jsx
+++ packages/ui/src/components/Section/SectionItem.jsx
@@ -42,7 +42,7 @@ function SectionItem({
   return (
     <Grid item xs={12}>
       <Element name={definition.id}>
-        <Card elevation={0}>
+        <Card elevation={0} variant="outlined">
           <ErrorBoundary>
             <CardHeader
               classes={{
@@ -75,6 +75,7 @@ function SectionItem({
               }
               subheader={
                 <Typography
+                  variant="body2"
                   className={classNames(
                     classes.description,
                     classes.descriptionHasData,
