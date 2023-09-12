b9d9f2c13c3c6a83247bd6068e9197790d7cc28c
Carlos Cruz <me@carcruzcast.com>
2023-09-12 23:48:40 +0100

    fix: remove old code

--- packages/sections/src/components/AssociationsToolkit/index.js
+++
@@ -1,9 +0,0 @@
-export { default as AdvanceOptionsMenu } from './AdvanceOptionsMenu';
-export { default as TableAssociations } from './Table/TableAssociations';
-export { default as TargetPrioritisationSwitch } from './TargetPrioritisationSwitch';
-export { default as SearhInput } from './SearchInput';
-export { default as DataDownloader } from './DataDownloader';
-export {
-  default as AssociationsContext,
-  AssociationsProvider,
-} from './context/AssociationsContext';
