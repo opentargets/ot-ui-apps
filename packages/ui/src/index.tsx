c0a334b6732956c9d089c97c595331e81272c03b
Carlos Cruz <me@carcruzcast.com>
2023-09-13 03:35:36 +0100

    fix: add missing import

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -5,6 +5,7 @@ export { default as GlobalSearch } from "./components/GlobalSearch";
 export { default as AutocompleteSearch } from "./components/AutocompleteSearch";
 export { default as SearchProvider } from "./components/Search/SearchContext";
 export { default as LongText } from "./components/LongText";
+export { default as LongList } from "./components/LongList";
 export { default as SummaryContainer } from "./components/Summary/SummaryContainer";
 export { default as SummaryItem } from "./components/Summary/SummaryItem";
 export { default as Tooltip } from "./components/Tooltip";
