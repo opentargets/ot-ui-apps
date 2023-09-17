4d353294f8e1ff1964876dcd0728898b319b5694
Riya Sinha <33243383+riyavsinha@users.noreply.github.com>
2023-09-17 14:21:42 -0700

    [Genetics] Add types to downloadtable helper  (#133)

--- apps/genetics/src/ot-ui-components/helpers/downloadTable.ts
+++ apps/genetics/src/ot-ui-components/helpers/downloadTable.ts
@@ -1,25 +1,45 @@
-import FileSaver from 'file-saver';
+import * as FileSaver from 'file-saver';
+
+export type TableRow = { [key: string]: any };
+export type HeaderMap = {
+  id: string;
+  label: string;
+  export?: (o: Object) => any;
+}[];
+export type DownloadFormat = 'csv' | 'tsv' | 'json';
 
 const UNEXPECTED_FORMAT =
   'Unexpected format. Supported options are csv, tsv and json.';
 
-const pick = (object, keys) => {
+const pick = (object: TableRow, keys: HeaderMap) => {
   return keys.reduce(function (o, k) {
     // take into account optional export() function, which takes precedence as per other download formats
     o[k.id] = k.export ? k.export(object) : object[k.id];
     return o;
-  }, {});
+  }, {} as TableRow);
 };
 
-const quoteIfString = d => (typeof d === 'string' ? `"${d}"` : d);
+const quoteIfString = (d: any) => (typeof d === 'string' ? `"${d}"` : d);
 
-const asJSONString = ({ rows, headerMap }) => {
+const asJSONString = ({
+  rows,
+  headerMap,
+}: {
+  rows: TableRow[];
+  headerMap: HeaderMap;
+}) => {
   // use the full headerMap which contain optional export() function for each header
   const rowsHeadersOnly = rows.map(row => pick(row, headerMap));
   return JSON.stringify(rowsHeadersOnly);
 };
 
-const asCSVString = ({ rows, headerMap }) => {
+const asCSVString = ({
+  rows,
+  headerMap,
+}: {
+  rows: TableRow[];
+  headerMap: HeaderMap;
+}) => {
   const separator = ',';
   const lineSeparator = '\n';
   const headersString = headerMap
@@ -37,7 +57,13 @@ const asCSVString = ({ rows, headerMap }) => {
   return [headersString, ...rowsArray].join(lineSeparator);
 };
 
-const asTSVString = ({ rows, headerMap }) => {
+const asTSVString = ({
+  rows,
+  headerMap,
+}: {
+  rows: TableRow[];
+  headerMap: HeaderMap;
+}) => {
   const separator = '\t';
   const lineSeparator = '\n';
   const headersString = headerMap.map(d => d.label).join(separator);
@@ -51,7 +77,15 @@ const asTSVString = ({ rows, headerMap }) => {
   return [headersString, ...rowsArray].join(lineSeparator);
 };
 
-const asContentString = ({ rows, headerMap, format }) => {
+const asContentString = ({
+  rows,
+  headerMap,
+  format,
+}: {
+  rows: TableRow[];
+  headerMap: HeaderMap;
+  format: DownloadFormat;
+}) => {
   switch (format) {
     case 'json':
       return asJSONString({ rows, headerMap });
@@ -64,7 +98,7 @@ const asContentString = ({ rows, headerMap, format }) => {
   }
 };
 
-const asMimeType = format => {
+const asMimeType = (format: DownloadFormat) => {
   switch (format) {
     case 'json':
       return 'application/json;charset=utf-8';
@@ -77,17 +111,18 @@ const asMimeType = format => {
   }
 };
 
+type DownloadTableArgs = {
+  rows: TableRow[];
+  headerMap: HeaderMap;
+  format: DownloadFormat;
+  filenameStem: string;
+};
 const downloadTable = ({
   rows,
   headerMap,
   format,
   filenameStem,
-}: {
-  rows: unknown[];
-  headerMap: unknown;
-  format: unknown;
-  filenameStem: string;
-}) => {
+}: DownloadTableArgs) => {
   if (!rows || rows.length === 0) {
     console.info('Nothing to download.');
     return;
