c2627f1f66ec425b69975ffe49085dba2602c01c
Carlos Cruz <me@carcruzcast.com>
2023-09-22 15:00:01 +0100

    [Platform]: add prioritisation columns and labels (#251)

--- apps/platform/src/components/AssociationsToolkit/static_datasets/prioritizationCols.js
+++ apps/platform/src/components/AssociationsToolkit/static_datasets/prioritizationCols.js
@@ -8,72 +8,6 @@ const cols = [
     docsLink: 'http://home.opentargets.org/aotf-documentation#target-in-clinic',
   },
   {
-    id: 'isInMembrane',
-    label: 'Membrane protein',
-    aggregation: 'Tractability',
-    sectionId: 'subcellularLocation',
-    description: 'Target is annotated to be located in the cell membrane',
-    docsLink: 'http://home.opentargets.org/aotf-documentation#membrane-protein',
-  },
-  {
-    id: 'isSecreted',
-    label: 'Secreted protein',
-    aggregation: 'Tractability',
-    sectionId: 'subcellularLocation',
-    description: 'Target is annotated to be secreted',
-    docsLink: 'http://home.opentargets.org/aotf-documentation#secreted-protein',
-  },
-  {
-    id: 'hasLigand',
-    label: 'Ligand binder',
-    aggregation: 'Tractability',
-    sectionId: 'tractability',
-    description: 'Target binds a specific ligand',
-    docsLink: 'http://home.opentargets.org/aotf-documentation#ligand-binder',
-  },
-  {
-    id: 'hasPocket',
-    label: 'Predicted pockets',
-    aggregation: 'Tractability',
-    sectionId: 'tractability',
-    description: 'Target has predicted pockets',
-    docsLink:
-      'http://home.opentargets.org/aotf-documentation#predicted-pockets',
-  },
-  {
-    id: 'hasMouseKO',
-    label: 'Mouse KO',
-    aggregation: 'Doability',
-    sectionId: 'mousePhenotypes',
-    description: 'Availability of mouse knockout models for the target',
-    docsLink: 'http://home.opentargets.org/aotf-documentation#mouse-ko',
-  },
-  {
-    id: 'mouseOrthologMaxIdentityPercentage',
-    label: 'Mouse ortholog identity',
-    aggregation: 'Doability',
-    sectionId: 'compGenomics',
-    description: 'Mouse ortholog maximum identity percentage',
-    docsLink:
-      'http://home.opentargets.org/aotf-documentation#mouse-ortholog-identity',
-  },
-  {
-    id: 'hasHighQualityChemicalProbes',
-    label: 'Chemical probes',
-    aggregation: 'Doability',
-    sectionId: 'chemicalProbes',
-    description: 'Availability of high quality chemical probes for the target',
-    docsLink: 'http://home.opentargets.org/aotf-documentation#chemical-probes',
-  },
-  // {
-  //   id: 'hasTEP',
-  //   label: 'TEP',
-  //   aggregation: 'Doability',
-  //   sectionId: 'tractability',
-  //   description: 'Availability of Target Enabling Package for the target',
-  //   docsLink: 'https://partner-platform.opentargets.org/projects',
-  // },
-  {
     id: 'geneticConstraint',
     label: 'Genetic constraint',
     aggregation: 'Safety',
@@ -82,6 +16,15 @@ const cols = [
       'Relative genetic constraint in natural populations derived from GnomAD',
     docsLink:
       'http://home.opentargets.org/aotf-documentation#genetic-constraint',
+  },
+  {
+    id: 'geneEssentiality',
+    label: 'Gene essentiality',
+    aggregation: 'Safety',
+    sectionId: 'depMapEssentiality',
+    description: 'Gene is defined as core essential by the DepMap portal',
+    docsLink:
+      'http://home.opentargets.org/aotf-documentation#gene-essentiality',
   },
   {
     id: 'hasSafetyEvent',
@@ -130,6 +73,81 @@ const cols = [
     docsLink:
       'http://home.opentargets.org/aotf-documentation#tissue-distribution',
   },
+  {
+    id: 'isInMembrane',
+    label: 'Membrane protein',
+    aggregation: 'Tractability',
+    sectionId: 'subcellularLocation',
+    description: 'Target is annotated to be located in the cell membrane',
+    docsLink: 'http://home.opentargets.org/aotf-documentation#membrane-protein',
+  },
+  {
+    id: 'isSecreted',
+    label: 'Secreted protein',
+    aggregation: 'Tractability',
+    sectionId: 'subcellularLocation',
+    description: 'Target is annotated to be secreted',
+    docsLink: 'http://home.opentargets.org/aotf-documentation#secreted-protein',
+  },
+  {
+    id: 'hasLigand',
+    label: 'Ligand binder',
+    aggregation: 'Tractability',
+    sectionId: 'tractability',
+    description: 'Target binds a specific ligand',
+    docsLink: 'http://home.opentargets.org/aotf-documentation#ligand-binder',
+  },
+  {
+    id: 'hasSmallMoleculeBinder',
+    label: 'Small molecule binder',
+    aggregation: 'Tractability',
+    sectionId: 'tractability',
+    description: 'Target binds a small molecule',
+    docsLink:
+      'http://home.opentargets.org/aotf-documentation#small-molecule-binder',
+  },
+  {
+    id: 'hasPocket',
+    label: 'Predicted pockets',
+    aggregation: 'Tractability',
+    sectionId: 'tractability',
+    description: 'Target has predicted pockets',
+    docsLink:
+      'http://home.opentargets.org/aotf-documentation#predicted-pockets',
+  },
+  {
+    id: 'hasMouseKO',
+    label: 'Mouse KO',
+    aggregation: 'Doability',
+    sectionId: 'mousePhenotypes',
+    description: 'Availability of mouse knockout models for the target',
+    docsLink: 'http://home.opentargets.org/aotf-documentation#mouse-ko',
+  },
+  {
+    id: 'mouseOrthologMaxIdentityPercentage',
+    label: 'Mouse ortholog identity',
+    aggregation: 'Doability',
+    sectionId: 'compGenomics',
+    description: 'Mouse ortholog maximum identity percentage',
+    docsLink:
+      'http://home.opentargets.org/aotf-documentation#mouse-ortholog-identity',
+  },
+  {
+    id: 'hasHighQualityChemicalProbes',
+    label: 'Chemical probes',
+    aggregation: 'Doability',
+    sectionId: 'chemicalProbes',
+    description: 'Availability of high quality chemical probes for the target',
+    docsLink: 'http://home.opentargets.org/aotf-documentation#chemical-probes',
+  },
+  // {
+  //   id: 'hasTEP',
+  //   label: 'TEP',
+  //   aggregation: 'Doability',
+  //   sectionId: 'tractability',
+  //   description: 'Availability of Target Enabling Package for the target',
+  //   docsLink: 'https://partner-platform.opentargets.org/projects',
+  // },
 ];
 
 export default cols;
--- packages/ui/src/components/PartnerLockIcon.jsx
+++ packages/ui/src/components/PartnerLockIcon.jsx
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import Tooltip from "./Tooltip";
-// import config from '../config';
+import config from "../config";
 
-function PartnerLockIcon({ config }) {
+function PartnerLockIcon() {
   return (
     <Tooltip
       title={`The partner preview Platform contains all data found in the public Platform along with pre-publication data from Open Targets' projects and new features developed by the Platform team. Please do not share data or screenshots outside of the consortium partners. For more information, please email ${config.profile.helpdeskEmail}.`}
