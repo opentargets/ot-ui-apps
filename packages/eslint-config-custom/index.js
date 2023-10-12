5a8d7b042f89164bb3b37bcd6f3958880b2989ef
Carlos Cruz <me@carcruzcast.com>
2023-10-12 23:28:47 +0100

    fix: eslint configs

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,50 +1,36 @@
 module.exports = {
   env: {
+    node: true,
     browser: true,
-    es6: true,
-    jest: true,
   },
+  parser: "@typescript-eslint/parser",
   extends: [
-    "airbnb",
     "eslint:recommended",
-    "plugin:import/recommended",
-    "plugin:react/jsx-runtime",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
-    "turbo",
     "prettier",
   ],
-  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
     sourceType: "module",
+    ecmaVersion: 2020,
   },
-  plugins: ["react", "import", "@typescript-eslint", "react-hooks", "prettier"],
-  overrides: [
-    {
-      files: ["**/__tests__/**/*"],
-      env: {
-        jest: true,
-      },
+  settings: {
+    react: {
+      version: "detect",
     },
-  ],
+  },
   rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
     "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
     "no-restricted-exports": "off",
     "no-underscore-dangle": "warn",
     "no-await-in-loop": "warn",
     "no-param-reassign": ["error", { props: false }],
-    "import/prefer-default-export": "warn",
 
     "react/prop-types": 0,
     "react/react-in-jsx-scope": 0,
     "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
-
-    "jsx-a11y/no-static-element-interactions": "warn",
-    "jsx-a11y/click-events-have-key-events": "warn",
   },
 };
