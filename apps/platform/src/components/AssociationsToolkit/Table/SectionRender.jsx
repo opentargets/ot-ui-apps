0340f4eef13ddb333d79cc79ff56c60e4420a8cd
Chintan Mehta <22376522+chinmehta@users.noreply.github.com>
2023-11-21 22:56:12 +0000

    [Platform]: Added cancer hallmark to list of sections (#295)

--- apps/platform/src/components/AssociationsToolkit/Table/SectionRender.jsx
+++ apps/platform/src/components/AssociationsToolkit/Table/SectionRender.jsx
@@ -1,23 +1,23 @@
-import { Suspense } from 'react';
-import { styled } from '@mui/material/styles';
-import { LoadingBackdrop } from 'ui';
-import { ENTITIES } from '../utils';
+import { Suspense } from "react";
+import { styled } from "@mui/material/styles";
+import { LoadingBackdrop } from "ui";
+import { ENTITIES } from "../utils";
 
-import targetSections from '../../../sections/targetSections';
-import evidenceSections from '../../../sections/evidenceSections';
+import targetSections from "../../../sections/targetSections";
+import evidenceSections from "../../../sections/evidenceSections";
 
-const LoadingContainer = styled('div')({
-  margin: '25px 0',
-  height: '100px',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  gap: '20px',
+const LoadingContainer = styled("div")({
+  margin: "25px 0",
+  height: "100px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "20px",
 });
 
-const Container = styled('div')({
-  marginTop: '10px',
-  marginBottom: '40px',
+const Container = styled("div")({
+  marginTop: "10px",
+  marginBottom: "40px",
 });
 
 function LoadingSection() {
@@ -58,28 +58,31 @@ export function SectionRender({
   if (!flatCols.includes(expanded[1])) return null;
 
   switch (displayedTable) {
-    case 'prioritisations': {
+    case "prioritisations": {
       Component = targetSections.get(section);
       const { targetSymbol } = row.original;
       ensgId = entity === ENTITIES.DISEASE ? rowId : id;
       label = targetSymbol;
       componentId = ensgId;
-      entityOfSection = 'target';
+      entityOfSection = "target";
       break;
     }
-    case 'associations': {
+    case "associations": {
       Component = evidenceSections.get(section);
       const { diseaseName, targetSymbol } = row.original;
       ensgId = entity === ENTITIES.DISEASE ? rowId : id;
       efoId = entity === ENTITIES.DISEASE ? id : rowId;
       componentId = { ensgId, efoId };
       label = { symbol: targetSymbol, name: diseaseName };
-      entityOfSection = 'disease';
+      entityOfSection = "disease";
       break;
     }
     default:
       return <SectionNotFound />;
   }
+
+  if (!Component) return <SectionNotFound />;
+
   return <Component id={componentId} label={label} entity={entityOfSection} />;
 }
 
