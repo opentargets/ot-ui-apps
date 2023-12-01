c40a9d7472035d776fa110202cda7711f31c7219
Carlos Cruz <me@carcruzcast.com>
2023-12-01 02:17:10 +0000

    feat: wrap app with config provider

--- apps/platform/src/App.tsx
+++ apps/platform/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
-import { ThemeProvider, SearchProvider, PrivateRoute } from "ui";
+import { ThemeProvider, SearchProvider, PrivateRoute, ConfigurationProvider } from "ui";
 
 import SEARCH_QUERY from "./components/Search/SearchQuery.gql";
 import ShouldAccessPPP from "./components/ShouldAccessPPP";
@@ -23,54 +23,56 @@ function App() {
   const suggestions = getSuggestedSearch();
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <SearchProvider
-          searchSuggestions={suggestions}
-          searchQuery={SEARCH_QUERY}
-          searchPlaceholder="Search for a target, drug, disease, or phenotype..."
-        >
-          <Router>
-            <Switch>
-              <Route exact path="/">
-                <HomePage suggestions={suggestions} />
-              </Route>
-              <Route path="/search">
-                <SearchPage />
-              </Route>
-              <Route path="/downloads">
-                <DownloadsPage />
-              </Route>
-              <Route path="/disease/:efoId">
-                <DiseasePage />
-              </Route>
-              <Route path="/target/:ensgId">
-                <TargetPage />
-              </Route>
-              <Route path="/drug/:chemblId">
-                <DrugPage />
-              </Route>
-              <Route path="/evidence/:ensgId/:efoId">
-                <EvidencePage />
-              </Route>
-              <Route path="/variants">
-                <VariantsPage />
-              </Route>
-              <Route path="/api">
-                <APIPage />
-              </Route>
-              <Route path="/projects">
-                <PrivateRoute>
-                  <ProjectsPage />
-                </PrivateRoute>
-              </Route>
-              <Route>
-                <NotFoundPage />
-              </Route>
-            </Switch>
-            <ShouldAccessPPP />
-          </Router>
-        </SearchProvider>
-      </ThemeProvider>
+      <ConfigurationProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <SearchProvider
+            searchSuggestions={suggestions}
+            searchQuery={SEARCH_QUERY}
+            searchPlaceholder="Search for a target, drug, disease, or phenotype..."
+          >
+            <Router>
+              <Switch>
+                <Route exact path="/">
+                  <HomePage suggestions={suggestions} />
+                </Route>
+                <Route path="/search">
+                  <SearchPage />
+                </Route>
+                <Route path="/downloads">
+                  <DownloadsPage />
+                </Route>
+                <Route path="/disease/:efoId">
+                  <DiseasePage />
+                </Route>
+                <Route path="/target/:ensgId">
+                  <TargetPage />
+                </Route>
+                <Route path="/drug/:chemblId">
+                  <DrugPage />
+                </Route>
+                <Route path="/evidence/:ensgId/:efoId">
+                  <EvidencePage />
+                </Route>
+                <Route path="/variants">
+                  <VariantsPage />
+                </Route>
+                <Route path="/api">
+                  <APIPage />
+                </Route>
+                <Route path="/projects">
+                  <PrivateRoute>
+                    <ProjectsPage />
+                  </PrivateRoute>
+                </Route>
+                <Route>
+                  <NotFoundPage />
+                </Route>
+              </Switch>
+              <ShouldAccessPPP />
+            </Router>
+          </SearchProvider>
+        </ThemeProvider>
+      </ConfigurationProvider>
     </ApolloProvider>
   );
 }
