cc686dcfc0ef52ccea47d969f84b4bfaa8c31458
Chintan Mehta <mchintan24@gmail.com>
2023-12-05 15:15:14 +0000

    mui update

--- apps/platform/src/components/Facets/Facet.jsx
+++ apps/platform/src/components/Facets/Facet.jsx
@@ -4,31 +4,27 @@ import {
   AccordionSummary,
   IconButton,
   Typography,
-} from '@mui/material';
-import {
-  faChevronRight,
-  faChevronDown,
-  faXmark,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+} from "@mui/material";
+import { faChevronRight, faChevronDown, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { TreeView } from '@mui/lab';
-import { makeStyles } from '@mui/styles';
+import { TreeView } from "@mui/x-tree-view";
+import { makeStyles } from "@mui/styles";
 
-import { hasAnyDescendantChecked } from './utils';
-import TreeLevel from './TreeLevel';
+import { hasAnyDescendantChecked } from "./utils";
+import TreeLevel from "./TreeLevel";
 
 const useStyles = makeStyles({
   accordionSummaryContent: {
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    margin: '0 !important',
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: "0 !important",
   },
   clearButtonRoot: {
-    height: 'unset',
+    height: "unset",
   },
   facetRoot: {
-    width: '100%',
+    width: "100%",
   },
 });
 
--- apps/platform/src/components/Facets/TreeItem.jsx
+++ apps/platform/src/components/Facets/TreeItem.jsx
@@ -1,61 +1,61 @@
-import { Checkbox, FormControlLabel, Typography } from '@mui/material';
-import { TreeItem as MUITreeItem } from '@mui/lab';
-import { makeStyles } from '@mui/styles';
-import { PartnerLockIcon } from 'ui';
+import { Checkbox, FormControlLabel, Typography } from "@mui/material";
+import { TreeItem as MUITreeItem } from "@mui/x-tree-view";
+import { makeStyles } from "@mui/styles";
+import { PartnerLockIcon } from "ui";
 
 const useStyles = makeStyles(theme => ({
   countLabel: {
-    fontSize: '.7rem',
-    fontWeight: 'bold',
+    fontSize: ".7rem",
+    fontWeight: "bold",
     color: theme.palette.grey[500],
   },
 
   formControlLabelRoot: {
-    minWidth: '85%',
+    minWidth: "85%",
   },
   FormControlLabelLabel: {
-    fontSize: '.9rem',
-    display: 'flex',
-    maxWidth: '100%',
-    paddingRight: '15%',
+    fontSize: ".9rem",
+    display: "flex",
+    maxWidth: "100%",
+    paddingRight: "15%",
   },
   treeItemRoot: {
-    '&:focus > $treeItemContent $treeItemLabel': {
-      backgroundColor: 'transparent',
+    "&:focus > $treeItemContent $treeItemLabel": {
+      backgroundColor: "transparent",
     },
-    '&$treeItemSelected > $treeItemContent $treeItemLabel': {
-      backgroundColor: 'transparent',
+    "&$treeItemSelected > $treeItemContent $treeItemLabel": {
+      backgroundColor: "transparent",
     },
-    '&$treeItemSelected:focus > $treeItemContent $treeItemLabel': {
-      backgroundColor: 'transparent',
+    "&$treeItemSelected:focus > $treeItemContent $treeItemLabel": {
+      backgroundColor: "transparent",
     },
-    '&$treeItemSelected > $treeItemContent $treeItemLabel:hover': {
-      backgroundColor: 'transparent',
+    "&$treeItemSelected > $treeItemContent $treeItemLabel:hover": {
+      backgroundColor: "transparent",
     },
   },
 
   treeItemContent: {
-    '&:hover': {
+    "&:hover": {
       backgroundColor: theme.palette.grey[200],
     },
   },
   treeItemSelected: {},
   treeItemLabel: {
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '91%',
-    '&:hover': {
-      backgroundColor: 'transparent',
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "space-between",
+    width: "91%",
+    "&:hover": {
+      backgroundColor: "transparent",
     },
   },
   treeItemLabelText: {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
   },
   treeItemLabelIcon: {
-    marginLeft: '0.5em',
+    marginLeft: "0.5em",
   },
 }));
 
@@ -98,7 +98,7 @@ function TreeItem({
                 color="primary"
                 checked={checked}
                 indeterminate={indeterminate}
-                sx={{ padding: '4px' }}
+                sx={{ padding: "4px" }}
               />
             }
             label={
