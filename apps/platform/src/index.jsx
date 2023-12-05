8b05cb2a8b435cd7efabdcc3b04761201aec173e
Carlos Cruz <me@carcruzcast.com>
2023-12-05 15:09:56 +0000

    [Platform]: Upload suggestions component (#313)

--- apps/platform/src/index.jsx
+++ apps/platform/src/index.jsx
@@ -1,12 +1,13 @@
-import * as ReactDOMClient from 'react-dom/client';
-import TagManager from 'react-gtm-module';
-import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
+import * as ReactDOMClient from "react-dom/client";
+import TagManager from "react-gtm-module";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
 
-import App from './App';
-import config from './config';
+import App from "./App";
+import config from "./config";
 
-import 'typeface-inter';
-import './index.scss';
+import "typeface-inter";
+import "typeface-roboto-mono";
+import "./index.scss";
 
 if (import.meta.env.MODE) {
   loadDevMessages();
@@ -17,5 +18,5 @@ if (config.googleTagManagerID) {
   TagManager.initialize({ gtmId: config.googleTagManagerID });
 }
 
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(<App />);
