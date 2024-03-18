4c6ab33d782fdcb1564ee1f89d96eed6ab4edb1f
Riya Sinha <riya.v.sinha@gmail.com>
2024-03-17 22:39:10 -0700

    refactor: type associated genes utils

--- apps/genetics/src/sections/variant/AssociatedGenes/utils.ts
+++ apps/genetics/src/sections/variant/AssociatedGenes/utils.ts
@@ -1,15 +1,32 @@
 import { scaleSqrt } from "d3";
+import {
+  DistanceElement,
+  FunctionalPredictionElement,
+  GeneForVariant,
+} from "../../../__generated__/graphql";
 
 export const OVERVIEW = "overview";
 
 export const radiusScale = scaleSqrt().domain([0, 1]).range([0, 6]);
 
-export const getDataAll = genesForVariant => {
-  const data = [];
+export type GeneForVariantDataRowType =
+  | DistanceElement
+  | FunctionalPredictionElement
+  | number
+  | string;
+export type GeneForVariantDataRow = {
+  geneId: string;
+  geneSymbol: string;
+  overallScore: number;
+  [key: string]: GeneForVariantDataRowType;
+};
+
+export const getDataAll = (genesForVariant: GeneForVariant[]) => {
+  const data = [] as GeneForVariantDataRow[];
   genesForVariant.forEach(item => {
-    const row = {
+    const row: GeneForVariantDataRow = {
       geneId: item.gene.id,
-      geneSymbol: item.gene.symbol,
+      geneSymbol: item.gene.symbol || "",
       overallScore: item.overallScore,
     };
     // for distances we want to use the first element of
@@ -33,19 +50,41 @@ export const getDataAll = genesForVariant => {
   return data;
 };
 
-export const getDataAllDownload = tableData => {
+export const getDataAllDownload = (tableData: GeneForVariantDataRow[]) => {
   return tableData.map(row => {
     const newRow = { ...row };
-    if (row.canonical_tss) {
-      newRow.canonical_tss = row.canonical_tss.tissues[0].distance;
+    if (row.canonical_tss && isDistanceElement(row.canonical_tss)) {
+      newRow.canonical_tss = row.canonical_tss.tissues[0].distance || "";
     }
-    if (row.vep) {
-      newRow.vep = row.vep.tissues[0].maxEffectLabel;
+    if (row.vep && isFunctionalPredictionElement(row.vep)) {
+      newRow.vep = row.vep.tissues[0].maxEffectLabel || "";
     }
     return newRow;
   });
 };
 
-export const isDisabledColumn = (allData, sourceId) => {
+export const isDisabledColumn = (allData: GeneForVariantDataRow[], sourceId: string) => {
   return !allData.some(d => d[sourceId]);
+};
+
+/** Type Guards */
+
+export const isDistanceElement = (
+  element: GeneForVariantDataRowType
+): element is DistanceElement => {
+  const e = element as DistanceElement;
+  if (e.tissues) {
+    return e.tissues.length > 0 ? e.tissues[0].distance !== undefined : true;
+  }
+  return false;
+};
+
+export const isFunctionalPredictionElement = (
+  element: GeneForVariantDataRowType
+): element is FunctionalPredictionElement => {
+  const e = element as FunctionalPredictionElement;
+  if (e.tissues) {
+    return e.tissues.length > 0 ? e.tissues[0].maxEffectLabel !== undefined : true;
+  }
+  return false;
 };
