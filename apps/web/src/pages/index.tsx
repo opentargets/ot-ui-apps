504f715ddba30e75efb0f65aff141610c3e6c98f
Carlos Cruz <me@carcruzcast.com>
2024-03-27 13:51:58 +0000

    feat: typescript and eslint config

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Link } from "ui";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <Link to="fneofen">Hello</Link>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -60,9 +62,7 @@ export default function Home() {
             <h2>
               Docs <span>-&gt;</span>
             </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
+            <p>Find in-depth information about Next.js features and&nbsp;API.</p>
           </a>
 
           <a
@@ -74,9 +74,7 @@ export default function Home() {
             <h2>
               Learn <span>-&gt;</span>
             </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
+            <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
           </a>
 
           <a
@@ -88,9 +86,7 @@ export default function Home() {
             <h2>
               Templates <span>-&gt;</span>
             </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
+            <p>Discover and deploy boilerplate example Next.js&nbsp;projects.</p>
           </a>
 
           <a
@@ -102,10 +98,7 @@ export default function Home() {
             <h2>
               Deploy <span>-&gt;</span>
             </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.</p>
           </a>
         </div>
       </main>
