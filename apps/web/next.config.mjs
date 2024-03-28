599904455eb3b7867ada87b1650f45d3839e6f4b
Carlos Cruz <me@carcruzcast.com>
2024-03-28 22:22:30 +0000

    feat: add .gql support

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["ui"],
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)/,
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
