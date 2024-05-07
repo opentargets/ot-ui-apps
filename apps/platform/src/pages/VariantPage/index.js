7c479f85fd9536682d9fd8f50542c93709f2a4d4
Graham McNeill <gjmcn@users.noreply.github.com>
2024-05-07 11:51:23 +0100

    [Platform] Add variant page (#365)
    * add variant page
    
    * add header to variant page
    
    * switch to typescript

--- apps/platform/src/pages/VariantPage/index.js
+++ apps/platform/src/pages/VariantPage/index.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 export { default } from "./VariantPage";
+=======
+// export { default } from "./VariantPage";
+export {default} from "./VariantPage"
+>>>>>>> 0e1f5ab0e ([Platform] Add variant page (#365))
