5e76d1b7ecb4db7bae459751df82fef9ad1c2889
Graham McNeill <gjmcn@users.noreply.github.com>
2024-06-19 13:50:57 +0100

    [Platform] Add QTL credible sets widget to variant page (#396)

--- apps/platform/src/pages/VariantPage/Profile.tsx
+++ apps/platform/src/pages/VariantPage/Profile.tsx
@@ -9,6 +9,7 @@ import {
 import InSilicoPredictorsSummary from "sections/src/variant/InSilicoPredictors/Summary";
 import EVASummary from "sections/src/variant/EVA/Summary";
 import UniProtVariantsSummary from "sections/src/variant/UniProtVariants/Summary";
+import QTLCredibleSetsSummary from "sections/src/variant/QTLCredibleSets/Summary";
 import GWASCredibleSetsSummary from "sections/src/variant/GWASCredibleSets/Summary";
 import PharmacogenomicsSummary from "sections/src/variant/Pharmacogenomics/Summary";
 
@@ -17,6 +18,7 @@ import ProfileHeader from "./ProfileHeader";
 const InSilicoPredictorsSection = lazy(() => import("sections/src/variant/InSilicoPredictors/Body"));
 const EVASection = lazy(() => import("sections/src/variant/EVA/Body"));
 const UniProtVariantsSection = lazy(() => import("sections/src/variant/UniProtVariants/Body"));
+const QTLCredibleSetsSection = lazy(() => import("sections/src/variant/QTLCredibleSets/Body"));
 const GWASCredibleSetsSection = lazy(() => import("sections/src/variant/GWASCredibleSets/Body"));
 const PharmacogenomicsSection = lazy(() => import("sections/src/variant/Pharmacogenomics/Body"));
 
@@ -24,6 +26,7 @@ const summaries = [
   InSilicoPredictorsSummary,
   EVASummary,
   UniProtVariantsSummary,
+  QTLCredibleSetsSummary,
   GWASCredibleSetsSummary,
   PharmacogenomicsSummary,
 ];
@@ -54,6 +57,7 @@ function Profile({ varId }: ProfileProps) {
         <InSilicoPredictorsSummary />
         <EVASummary />
         <UniProtVariantsSummary />
+        <QTLCredibleSetsSummary />
         <GWASCredibleSetsSummary />
         <PharmacogenomicsSummary />
       </SummaryContainer>
@@ -69,10 +73,12 @@ function Profile({ varId }: ProfileProps) {
           <UniProtVariantsSection id={varId} label='NO-LABEL!' entity={VARIANT} />
         </Suspense>
         <Suspense fallback={<SectionLoader />}>
+          <QTLCredibleSetsSection id={varId} label='NO-LABEL!' entity={VARIANT} />
+        </Suspense>
+        <Suspense fallback={<SectionLoader />}>
           <GWASCredibleSetsSection id={varId} label='NO-LABEL!' entity={VARIANT} />
           <PharmacogenomicsSection id={varId} label='NO-LABEL!' entity={VARIANT} />
         </Suspense>
-        {/* NEED ANYTHING IN <PrivateWrapper> ???? see evidence page */}
       </SectionContainer>
   
     </>
