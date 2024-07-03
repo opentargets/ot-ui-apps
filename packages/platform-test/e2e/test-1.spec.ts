5d14b391d691156891467451c3b842243f5dd0c7
Carlos Cruz <me@carcruzcast.com>
2024-07-03 21:48:48 +0100

    feat: update

--- packages/platform-test/e2e/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await page.getByRole("link", { name: "ACHE" }).click();
-  await page.getByRole("tab", { name: "Profile" }).click();
-});
