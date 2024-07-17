5258399efec3393cd12f8a9952552c70fdb2269e
Graham McNeill <graham.j.mcneill@gmail.com>
2024-07-17 15:05:10 +0100

    add metadata fields

--- apps/platform/src/pages/StudyPage/ProfileHeader.tsx
+++ apps/platform/src/pages/StudyPage/ProfileHeader.tsx
@@ -5,6 +5,7 @@ import {
   ProfileDescription,
   Field,
   ProfileHeader as BaseProfileHeader,
+  Tooltip,
 } from "ui";
 import { naLabel } from "../../constants";
 // import { Fragment } from "react";
@@ -57,14 +58,14 @@ function ProfileHeader({ studyId, studyType }) {
         </Field>
         <Field loading={loading} title="Journal">
           { 
-             studyType === "GWAS" || studyType === "QTL"
+            studyType === "GWAS" || studyType === "QTL"
               ? publicationJournal
               : naLabel
           } 
         </Field>
         <Field loading={loading} title="PubMed">
           { 
-             studyType === "GWAS" || studyType === "QTL"
+            studyType === "GWAS" || studyType === "QTL"
               ? <Link external to={`https://europepmc.org/article/med/${pubmedId}`}>
                   {pubmedId}
                 </Link>
@@ -73,26 +74,52 @@ function ProfileHeader({ studyId, studyType }) {
         </Field>
         <Field loading={loading} title="Has summary stats">
           { 
-             studyType === "GWAS"
+            studyType === "GWAS"
               ? (summarystatsLocation ? "yes" : "no")
               : "yes"
           } 
         </Field>
         <Field loading={loading} title="N study">
+          {nSamples}
         </Field>
         <Field loading={loading} title="N discovery">
+          { 
+            studyType === "GWAS"
+              ? initialSampleSize
+              : studyType === "FINNGEN"
+                ? discoverySamples.length  // USE LENGTH??? - AND NEED TO ADD initialSampleSize TOOLTIP HERE!!!
+                : naLabel
+          } 
         </Field>
-        <Field loading={loading} title="N replication">
+       <Field loading={loading} title="N replication">
+          { 
+            studyType === "GWAS"
+              ? replicationSamples.length  // ?? USE LENGTH
+              : naLabel
+          } 
         </Field>
         <Field loading={loading} title="N cases">
+          { 
+            studyType === "GWAS" || studyType === "FINNGEN"
+              ? nCases
+              : naLabel
+          } 
         </Field>
         <Field loading={loading} title="N controls">
+          { 
+            studyType === "GWAS" || studyType === "FINNGEN"
+              ? nControls
+              : naLabel
+          } 
         </Field>
         <Field loading={loading} title="Cohorts">
+          TODO
         </Field>
         <Field loading={loading} title="QC">
+          TODO
         </Field>
         <Field loading={loading} title="Study flags">
+          TODO
         </Field>
       </>
     </BaseProfileHeader>
