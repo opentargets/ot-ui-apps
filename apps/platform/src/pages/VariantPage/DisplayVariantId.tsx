9243fff88c84c75a8349f0e7028461d7061a9a97
Graham McNeill <graham.j.mcneill@gmail.com>
2024-08-01 13:21:10 +0100

    use dialog

--- apps/platform/src/pages/VariantPage/DisplayVariantId.tsx
+++ apps/platform/src/pages/VariantPage/DisplayVariantId.tsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { Box, Popover, Typography, IconButton, Snackbar } from "@mui/material";
+import {
+  Box,
+  Typography,
+  IconButton,
+  Snackbar,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+} from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faClipboard } from "@fortawesome/free-regular-svg-icons";
@@ -9,47 +17,47 @@ import { lighten } from "polished";
 const highlightBackground = theme => lighten(0.4, theme.palette.primary.main);
 
 type DisplayVariantIdProps = {
-  variandId: string;
+  variantId: string;
   referenceAllele: string;
   alternateAllele: string;
   maxChars?: number;
 };
 
-function DisplayVariantId({ variantId, referenceAllele, alternateAllele, maxChars = 5 }) {
+function DisplayVariantId({
+    variantId,
+    referenceAllele,
+    alternateAllele,
+    maxChars = 6}: DisplayVariantIdProps) {
 
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleClick = () => {
+    setOpen(true);
   };
 
   const handleClose = () => {
-    setAnchorEl(null);
+    setOpen(false);
   };
 
   function handleCloseSnackbar() {
     setSnackbarOpen(false);
   }
 
-  function copyToClipboard() {
+  function copyToClipboard(text: string) {
     setSnackbarOpen(true);
-    navigator.clipboard.writeText(fullName);
+    navigator.clipboard.writeText(text);
   }
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'popover' : undefined;
 
   const stem = variantId.split('_').slice(0, -1).join('_');
   const longReferenceAllele = referenceAllele.length > maxChars;
   const longAlternateAllele = alternateAllele.length > maxChars;
-  const fullName = `${stem}_${referenceAllele}_${alternateAllele}`;
+  const fullVariantId = `${stem}_${referenceAllele}_${alternateAllele}`;
 
   if (longReferenceAllele || longAlternateAllele) {
     return (
       <div>
         <Box
-          aria-describedby={id}
           onClick={handleClick}
           title="Show full variant ID"
           sx={{
@@ -73,32 +81,16 @@ function DisplayVariantId({ variantId, referenceAllele, alternateAllele, maxChar
             : alternateAllele
           }
         </Box>
-        <Popover
-          id={id}
+        <Dialog
           open={open}
-          anchorEl={anchorEl}
           onClose={handleClose}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          slotProps={{
-            paper: {
-              sx: { position: "relative" }
-            }
-          }}  
+          scroll="paper"
+          aria-labelledby="dialog-title"
+          aria-describedby="dialog-description"
+          maxWidth="md"
         >
-          <Box
-            position="fixed"
-            width="100%"
-            // display="flex"
-            // left={0}
-            // right={0}
-            bgcolor="white"
-            sx={{borderBottom: "1px solid #ccc"}}
-            zIndex={2}
-          >
-            <Typography variant="h6" padding="1rem">
+          <DialogTitle id="dialog-title">
+            <Typography variant="h6">
               Variant ID
             </Typography>
             <IconButton
@@ -113,54 +105,35 @@ function DisplayVariantId({ variantId, referenceAllele, alternateAllele, maxChar
             >
               <FontAwesomeIcon icon={faXmark} />
             </IconButton>
-          </Box>
-          <Box p="5em 2em">
-            <Box 
-              sx={{
-                backgroundColor: theme => theme.palette.grey[300],
-                padding: "1em 3.5em 1em 1em",   
-                fontSize: "0.9em",
-                position: "relative"
-              }}
-            >
-              <Tooltip
-                title="Copy JSON to clipboard"
-              >
-                <IconButton
-                  onClick={copyToClipboard}
-                  sx={{
-                    position: "absolute !important",
-                    top: "0",
-                    right: "0",
-                    padding: "0.4em 0.5em !important",
-                  }}
-                >
-                  <FontAwesomeIcon icon={faClipboard} />
-                </IconButton>
-              </Tooltip>
-              <Typography
-                variant="body2"
-                sx={{
-                  textWrap: "wrap",
-                  wordWrap: "break-word",
-                }}
-              >
-                {fullName}
-              </Typography>
-            </Box>
-          </Box>
-        </Popover>
+          </DialogTitle>
+          <DialogContent
+            dividers={true}
+            sx={{ padding: "0 1.5em 3em" }}
+          >
+            <CopyPanel
+              heading="Hashed Variant ID"
+              tooltipText="The variant ID used in Open Targets data."
+              text={variantId}
+              copyToClipboard={copyToClipboard}
+            />
+            <CopyPanel
+              heading="Full Variant ID"
+              text={fullVariantId}
+              copyToClipboard={copyToClipboard}
+            />
+          </DialogContent>
+        </Dialog>
         <Snackbar
           open={snackbarOpen}
           onClose={handleCloseSnackbar}
-          message="Variant ID copied"
+          message="Copied to clipboard"
           autoHideDuration={3000}
         />
       </div>
     );
   }
 
-  return fullName;
+  return fullVariantId;
 
 }
 
@@ -174,6 +147,47 @@ function HighlightBox({ children }) {
       bgcolor={highlightBackground}
     >
       {children}
+    </Box>
+  );
+}
+
+function CopyPanel({ heading, text, tooltipText, copyToClipboard }) {
+  return (
+    <Box mt={2}>
+      {
+        tooltipText
+          ? <Tooltip title={tooltipText} showHelpIcon>
+              <Typography variant="subtitle1" component="span">{heading}</Typography>
+            </Tooltip>
+          : <Typography variant="subtitle1">{heading}</Typography>
+      }
+      <Box sx={{
+          marginTop: '0.1em',
+          backgroundColor: theme => theme.palette.grey[300],
+          position: "relative",
+        }}
+      >
+        <Box position="absolute" top={0} right={0}>
+          <Tooltip title="Copy to clipboard">
+            <IconButton
+              onClick={() => copyToClipboard(text)}
+              sx={{ padding: "0.4em 0.5em !important" }}
+            >
+              <FontAwesomeIcon icon={faClipboard} />
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Typography
+          variant="body2"
+          sx={{
+            padding: "1em 3.2em 1em 1em",   
+            textWrap: "wrap",
+            wordWrap: "break-word",
+          }}
+        >
+          {text}
+        </Typography>
+      </Box>
     </Box>
   );
 }
