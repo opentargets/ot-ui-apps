c7c90cbb2665ce2717352c45845a8bf516522847
Graham McNeill <graham.j.mcneill@gmail.com>
2024-09-04 12:14:42 +0100

    use credibleSets aliases in summary fragments

--- packages/sections/src/variant/GWASCredibleSets/index.ts
+++ packages/sections/src/variant/GWASCredibleSets/index.ts
@@ -3,5 +3,6 @@ export const definition = {
   id,
   name: "GWAS Credible Sets",
   shortName: "GW",
-  hasData: data => data?.credibleSets?.length > 0,
+  hasData: data => data?.gwasCredibleSets?.length > 0 ||
+                   data?.credibleSets?.length > 0,
 };
--- packages/sections/src/variant/QTLCredibleSets/index.ts
+++ packages/sections/src/variant/QTLCredibleSets/index.ts
@@ -3,5 +3,6 @@ export const definition = {
   id,
   name: "molQTL Credible Sets",
   shortName: "QT",
-  hasData: data => data?.credibleSets?.length > 0,
+  hasData: data => data?.qtlCredibleSets?.length > 0 ||
+                   data?.credibleSets?.length > 0,
 };
