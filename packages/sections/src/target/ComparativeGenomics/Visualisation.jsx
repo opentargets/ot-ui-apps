72e171cd8613473cc97bf3bf412a98637d9d5b44
HelenaCornu <hcornu@ebi.ac.uk>
2024-09-10 16:00:53 +0100

    updates to visualisation

--- packages/sections/src/target/ComparativeGenomics/Visualisation.jsx
+++ packages/sections/src/target/ComparativeGenomics/Visualisation.jsx
@@ -1,5 +1,20 @@
 import * as d3 from "d3";
 import { useRef, useEffect } from "react";
+const labels = {
+  "6239": "",
+  "7227": "",
+  "7955": "",
+  "8364": "",
+  "9823": "",
+  "9615": "",
+  "10141": "",
+  "9986": "",
+  "10116": "",
+  "10090": "",
+  "9544": "",
+  "9598": "",
+  "9606": "",
+}
 
 function Visualisation({ homologues }) {
   const containerReference = useRef();
@@ -16,33 +31,61 @@ function Visualisation({ homologues }) {
     const queryScale = d3
       .scaleLinear()
       .domain([0, 100])
-      .range([marginLeft, (width - marginRight) / 2]);
+      .range([marginLeft, (width - marginRight) / 3]);
 
     const targetScale = d3
       .scaleLinear()
       .domain([100, 0])
-      .range([marginLeft + (width - marginRight) / 2, width - marginRight]);
+      .range([marginLeft + (width - marginRight) / 1.5, width - marginRight]);
 
     // Declare the y (vertical position) scale.
+
+    // "6239",
+    // "7227",
+    // "7955",
+    // "8364",
+    // "9823",
+    // "9615",
+    // "10141",
+    // "9986",
+    // "10116",
+    // "10090",
+    // "9544",
+    // "9598",
+    // "9606",
+
+    // "Human",
+    //   "Chimpanzee",
+    //   "Macaque",
+    //   "Mouse",
+    //   "Rat",
+    //   "Rabbit",
+    //   "Guinea Pig",
+    //   "Dog",
+    //   "Pig",
+    //   "Tropical clawed frog",
+    //   "Zebrafish",
+    //   "Drosophila melanogaster (Fruit fly)",
+    //   "Caenorhabditis elegans (Nematode, N2)"
 
     const y = d3
       .scalePoint()
       .domain([
-        "9598",
-        "10116",
-        "9606",
-        "8364",
-        "9615",
-        "7227",
-        "9986",
-        "9544",
-        "9823",
         "6239",
+        "7227",
         "7955",
+        "8364",
+        "9823",
+        "9615",
         "10141",
+        "9986",
+        "10116",
         "10090",
+        "9544",
+        "9598",
+        "9606",
       ])
-      .range([height - marginBottom, marginTop]);
+      .range([height - marginBottom*2, marginTop]);
 
     // Create the SVG container.
     const svg = d3.create("svg").attr("width", width).attr("height", height);
@@ -50,20 +93,39 @@ function Visualisation({ homologues }) {
     // Add the x-axis.
     svg
       .append("g")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(d3.axisBottom(queryScale));
+      .attr("transform", `translate(0,${height-marginBottom})`)
+      .call(d3.axisBottom(queryScale))
+      .call(g => g.append("text")
+        .attr("x", marginLeft)
+        .attr("y", marginBottom - 1)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("Query Percentage Identity →"));
 
     // Add the x-axis.
     svg
       .append("g")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(d3.axisBottom(targetScale));
+      .attr("transform", `translate(0,${height-marginBottom})`)
+      .call(d3.axisBottom(targetScale))
+      .call(g => g.append("text")
+        .attr("x", width-marginRight)
+        .attr("y", marginBottom - 1)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "end")
+        .text("← Target Percentage Identity"));
 
+
+    const yAxis = d3.axisLeft(y).tickFormat((d) => labels[d])
     // Add the y-axis.
-    svg.append("g").attr("transform", `translate(${marginLeft},0)`).call(d3.axisLeft(y));
+    svg
+    .append("g")
+    .attr("transform", `translate(${width / 2},0)`)
+    .call(yAxis)
+    .call(g => g.select(".domain").remove());
 
+    // Create the grid
     const queryContainer = svg.append("g").attr("class", "queryContainer");
-    const targetContainer = svg.append("g").attr("class", "targuetContainer");
+    const targetContainer = svg.append("g").attr("class", "targetContainer");
 
     queryContainer
       .selectAll("query")
@@ -76,10 +138,10 @@ function Visualisation({ homologues }) {
       .attr("cy", function (d) {
         return y(d.speciesId);
       })
-      .attr("r", 7)
+      .attr("r", 5)
       .attr("fill-opacity", 0.5)
-      .attr("fill", "#666")
-      .attr("stroke", "#2c2c2c");
+      .attr("fill", "#c2dcef")
+      .attr("stroke", "#1a4565");
 
     targetContainer
       .selectAll("target")
@@ -92,10 +154,10 @@ function Visualisation({ homologues }) {
       .attr("cy", function (d) {
         return y(d.speciesId);
       })
-      .attr("r", 7)
+      .attr("r", 5)
       .attr("fill-opacity", 0.5)
-      .attr("fill", "#666")
-      .attr("stroke", "#2c2c2c");
+      .attr("fill", "#c2dcef")
+      .attr("stroke", "#1a4565");
 
     // Append the SVG element.
     containerReference.current.append(svg.node());
