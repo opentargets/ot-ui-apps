ba9753168aca213221fc4cf3fe6cf4ba3cc460ba
Graham McNeill <gjmcn@users.noreply.github.com>
2024-10-03 18:34:26 +0100

    precision

--- apps/platform/src/pages/CredibleSetPage/ProfileHeader.tsx
+++ apps/platform/src/pages/CredibleSetPage/ProfileHeader.tsx
@@ -61,7 +61,7 @@ function ProfileHeader({ variantId }: ProfileHeaderProps) {
               </Tooltip>
             }
           >
-            {beta.toPrecision(3)}
+            {beta.toPrecision(2)}
           </Field>
         }
         {typeof standardError === 'number' &&
@@ -80,12 +80,12 @@ function ProfileHeader({ variantId }: ProfileHeaderProps) {
               </Tooltip>
             }
           >
-            {standardError.toPrecision(3)}
+            {standardError.toPrecision(2)}
           </Field>
         }
         {typeof credibleSet?.effectAlleleFrequencyFromSource === "number" &&
           <Field loading={loading} title="EAF">
-            {credibleSet.effectAlleleFrequencyFromSource.toPrecision(3)}
+            {credibleSet.effectAlleleFrequencyFromSource.toPrecision(2)}
           </Field>
         }
         {typeof leadVariant?.posteriorProbability === "number" &&
@@ -104,7 +104,7 @@ function ProfileHeader({ variantId }: ProfileHeaderProps) {
               </Tooltip>
             }
           >
-            {leadVariant.posteriorProbability.toPrecision(3)}
+            {leadVariant.posteriorProbability.toPrecision(2)}
           </Field>
         }
         <Field loading={loading} title="GRCh38">
@@ -139,7 +139,7 @@ function ProfileHeader({ variantId }: ProfileHeaderProps) {
           {credibleSet?.credibleSetIndex}
         </Field>
         <Field loading={loading} title="Purity min">
-          {credibleSet?.purityMinR2?.toPrecision(3)}
+          {credibleSet?.purityMinR2?.toPrecision(2)}
         </Field>
         <Field loading={loading} title="Start">
           {credibleSet?.locusStart}
