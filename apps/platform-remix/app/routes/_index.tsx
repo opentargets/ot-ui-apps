735ffa469924f893ba587452456d2bd94a67bf76
Carlos Cruz <me@carcruzcast.com>
2024-10-10 01:01:20 +0100

    feat: add remix app

---
+++ apps/platform-remix/app/routes/_index.tsx
@@ -0,0 +1,12 @@
+// import { CounterButton } from "@repo/ui/counter-button";
+// import { Link } from "@repo/ui/link";
+
+export default function Index(): JSX.Element {
+  return (
+    <div className="container">
+      <h1 className="title">
+        <span>Platform</span>
+      </h1>
+    </div>
+  );
+}
