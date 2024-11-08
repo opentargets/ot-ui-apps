1749470b431ef0420828944826e32274e1f03ede
David Ochoa <dogcaesar@gmail.com>
2024-11-08 16:03:48 +0000

    refactor: rename VEP widget (#526)

--- packages/sections/src/variant/VariantEffectPredictor/index.ts
+++ packages/sections/src/variant/VariantEffectPredictor/index.ts
@@ -1,7 +1,7 @@
 const id = "variant_effect_predictor";
 export const definition = {
   id,
-  name: "Variant Effect Predictor (VEP)",
+  name: "Transcript consequences",
   shortName: "VE",
   hasData: data => data?.transcriptConsequences?.length > 0,
 };
