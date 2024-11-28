526fb12c1ecf52ef492eb14189a776b0ae603da1
Graham McNeill <graham.j.mcneill@gmail.com>
2024-11-28 14:00:31 +0000

    remove console.log

--- packages/sections/src/disease/GWASStudies/index.ts
+++ packages/sections/src/disease/GWASStudies/index.ts
@@ -2,7 +2,7 @@ export const definition = {
   id: "GWASStudies",
   name: "GWAS Studies",
   shortName: "GS",
-  hasData: data => (console.log(data),
+  hasData: data => (
     data?.studies?.count > 0 ||  // summary
     data?.count > 0              // section
   ),
