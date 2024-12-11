4919f40827c0fecca8aa30c25b560e9be6927585
Carlos Cruz <me@carcruzcast.com>
2024-12-11 00:22:43 -0600

    fix: create alias to get full locus count independent of lead

--- apps/platform/src/pages/CredibleSetPage/Profile.tsx
+++ apps/platform/src/pages/CredibleSetPage/Profile.tsx
@@ -35,7 +35,7 @@ function Profile({ studyLocusId, variantId, referenceAllele, alternateAllele }: 
   );
 
   const CREDIBLE_SET_PROFILE_QUERY = gql`
-    query CredibleSetProfileQuery($studyLocusId: String!) {
+    query CredibleSetProfileQuery($studyLocusId: String!, $variantIds: [String!]!) {
       credibleSet(studyLocusId: $studyLocusId) {
         studyLocusId
         ...CredibleSetProfileHeaderFragment
@@ -50,7 +50,7 @@ function Profile({ studyLocusId, variantId, referenceAllele, alternateAllele }: 
     <PlatformApiProvider
       entity={CREDIBLE_SET}
       query={CREDIBLE_SET_PROFILE_QUERY}
-      variables={{ studyLocusId: studyLocusId }}
+      variables={{ studyLocusId: studyLocusId, variantIds: [variantId] }}
       client={client}
     >
       <ProfileHeader />
