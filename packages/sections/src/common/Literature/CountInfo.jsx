52c16103698c6fd0c6331a73b6b7ef5560860a9c
Graham McNeill <gjmcn@users.noreply.github.com>
2025-01-31 14:27:17 +0000

    [Platform] Use React for state management in literature section (#416)
    * duplicate literature section
    
    * use react for state management
    
    * fix: move PMC lit details to context
    
    * remove comments
    
    * show new and old widget on all pages
    
    * remove old files
    
    * uncomment sections
    
    * add page to LiteratureStateType
    
    * undo whitespace changes
    
    * undo whitespace changes
    
    * cleanup and improve types
    
    * finish cleaning up types and remove recoil
    
    * remove console logs

--- packages/sections/src/common/Literature/CountInfo.jsx
+++ packages/sections/src/common/Literature/CountInfo.jsx
@@ -1,29 +1,16 @@
 import { InputLabel, Box } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import { useRecoilValue } from "recoil";
-import { litsCountState, loadingEntitiesState, tablePageSizeState } from "./atoms";
-
-const useStyles = makeStyles(() => ({
-  resultCount: {
-    // marginLeft: "2rem",
-    // marginRight: "6rem",
-  },
-}));
+import { useLiterature } from "./LiteratureContext";
 
 function CountInfo() {
-  const classes = useStyles();
-  const pageSize = useRecoilValue(tablePageSizeState);
-  const count = useRecoilValue(litsCountState);
-  const loadingEntities = useRecoilValue(loadingEntitiesState);
-
+  const { pageSize, litsCount, loadingEntities } = useLiterature();
   const getLabelText = () => {
     if (loadingEntities) return "Loading count...";
-    return `Showing ${count > pageSize ? pageSize : count} of ${count} results`;
+    return `Showing ${litsCount > pageSize ? pageSize : litsCount} of ${litsCount} results`;
   };
 
   return (
     <Box sx={{ mt: 4, mr: 3 }}>
-      <InputLabel className={classes.resultCount}>{getLabelText()}</InputLabel>
+      <InputLabel>{getLabelText()}</InputLabel>
     </Box>
   );
 }
