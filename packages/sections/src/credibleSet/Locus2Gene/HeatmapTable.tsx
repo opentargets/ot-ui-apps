bb318747c7da74b5fc3501e3c5dbb606882bf6f8
Graham McNeill <graham.j.mcneill@gmail.com>
2025-02-13 11:00:03 +0000

    polish cell plot

--- packages/sections/src/credibleSet/Locus2Gene/HeatmapTable.tsx
+++ packages/sections/src/credibleSet/Locus2Gene/HeatmapTable.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import * as PlotLib from "@observablehq/plot";
-import { interpolateRdBu, schemeRdBu, scaleDiverging } from "d3";
+import { interpolateRdBu, schemeRdBu, scaleDiverging, rgb, sum } from "d3";
 import { ObsPlot } from "ui";
 import { Box, Typography, Popover } from "@mui/material";
 
@@ -13,23 +13,19 @@ function HeatmapTable() {
 
   const theadElement = (
     <thead>
-      <tr>
+      <Box component="tr" sx={{}}>
         <th></th>
         <th></th>
         <th></th>
-        <Box
-          component="th"
-          colSpan="3"
-          sx={{ borderBottom: "1px solid #888", paddingBottom: "3px" }}
-        >
+        <th></th>
+        <Box component="th" colSpan="3" sx={{ borderBottom: "1px solid #888", paddingBottom: 1 }}>
           <Typography variant="subtitle2">Colocalisation</Typography>
         </Box>
         <th></th>
         <th></th>
-        <th></th>
-      </tr>
+      </Box>
       <tr>
-        {["Gene", ...Object.keys(groupToFeature), "Base", "L2G"].map((value, index) => (
+        {["Gene", "L2G score", ...Object.keys(groupToFeature), "Base"].map((value, index) => (
           <HeaderCell key={index} value={value} />
         ))}
       </tr>
@@ -41,6 +37,7 @@ function HeatmapTable() {
       {groupResults.map(row => (
         <tr key={row.geneId}>
           <GeneCell value={row.geneId} />
+          <ScoreCell value={row.totalL2GScore?.toFixed(3)} />
           {Object.keys(groupToFeature).map(groupName => {
             return (
               <HeatCell
@@ -53,7 +50,6 @@ function HeatmapTable() {
             );
           })}
           <BaseCell value={row.shapBaseValue?.toFixed(3)} />
-          <ScoreCell value={row.totalL2GScore?.toFixed(3)} />
         </tr>
       ))}
     </tbody>
@@ -61,18 +57,20 @@ function HeatmapTable() {
 
   return (
     <Box display="flex" justifyContent="center">
-      <table
-        style={{
+      <Box
+        component="table"
+        sx={{
           tableLayout: "fixed",
           width: "90%",
           maxWidth: "1000px",
           borderCollapse: "separate",
-          borderSpacing: "4px",
+          borderSpacing: "6px",
+          my: 4,
         }}
       >
         {theadElement}
         {tbodyElement}
-      </table>
+      </Box>
     </Box>
   );
 }
@@ -81,7 +79,7 @@ export default HeatmapTable;
 
 function HeaderCell({ value }) {
   return (
-    <Box component="th">
+    <Box component="th" pt={1}>
       <Typography variant="subtitle2">{value}</Typography>
     </Box>
   );
@@ -113,12 +111,13 @@ function HeatCell({ value, bgrd, geneId, groupName }) {
         component="td"
         bgcolor={bgrd}
         borderRadius={1.5}
-        padding={1}
+        padding={1.5}
         onClick={handleClick}
         sx={{
           outline: anchorEl ? "2px solid #000" : "none",
           "&:hover": {
             outline: `2px solid #888`,
+            cursor: "pointer",
           },
         }}
       >
@@ -152,15 +151,15 @@ function HeatCell({ value, bgrd, geneId, groupName }) {
           mt: 0.5,
         }}
       >
-        <ObsPlot
-          data={getTargetGroupFeatures(geneId, groupName)}
-          otherData={{ groupName }}
-          // height={100}
-          minWidth={490}
-          maxWidth={490}
-          renderChart={renderPopoverChart}
-        />
-        {/* <Typography sx={{ p: 2 }}>The content of the Popover.</Typography> */}
+        <Box sx={{ px: 3, py: 2 }}>
+          <ObsPlot
+            data={getTargetGroupFeatures(geneId, groupName)}
+            otherData={{ groupName }}
+            minWidth={530}
+            maxWidth={530}
+            renderChart={renderPopoverChart}
+          />
+        </Box>
       </Popover>
     </>
   );
@@ -168,7 +167,7 @@ function HeatCell({ value, bgrd, geneId, groupName }) {
 
 function BaseCell({ value }) {
   return (
-    <Box component="td" borderRadius={1.5} padding={1} border="1px solid #bbb">
+    <Box component="td" borderRadius={1.5} padding={1.5} border="1px solid #bbb">
       <Typography
         fontSize={13}
         color="#777"
@@ -184,7 +183,7 @@ function BaseCell({ value }) {
 
 function ScoreCell({ value }) {
   return (
-    <Box component="td" borderRadius={1.5} padding={1}>
+    <Box component="td" borderRadius={1.5} padding={1.5}>
       <Typography
         fontSize={13}
         fontWeight={700}
@@ -200,22 +199,25 @@ function ScoreCell({ value }) {
 
 function renderPopoverChart({ data, otherData: { groupName }, width, height }) {
   const textInBarCutoff = 0.4; // hacky!
+  const dxName = -70;
+  const dxValue = -18;
+  const dyHeader = -25;
 
   return PlotLib.plot({
     width,
     height,
-    marginLeft: 280,
-    marginRight: 20,
-    marginTop: 10,
+    marginLeft: 310,
+    marginRight: 10,
+    marginTop: 30,
     marginBottom: 30,
     style: {
-      fontSize: 11,
-      // background: "#fbfbfb",
+      fontSize: 12,
     },
     x: {
       domain: [-1, 1],
       label: null,
-      // ticks: [-1, -0.5, 0, 0.5, 1],
+      ticks: [-1, 0, 1],
+      tickFormat: Math.round,
     },
     y: {
       type: "band",
@@ -225,41 +227,99 @@ function renderPopoverChart({ data, otherData: { groupName }, width, height }) {
       grid: true,
       padding: 0.2,
       inset: 0.1,
-      tickFormat: value => "",
-      // tickFormat: value => (console.log(value), value),
+      tickPadding: -dxName,
     },
     marks: [
+      // x = 0 line
       PlotLib.ruleX([0], {
         strokeOpacity: 0.1,
       }),
-      // use text mark for y labels for flexibility
+
+      // column headers
+      PlotLib.text(data.slice(0, 1), {
+        x: -1,
+        y: "name",
+        dx: dxName,
+        dy: dyHeader,
+        text: d => "Feature",
+        fontWeight: 500,
+        textAnchor: "end",
+      }),
+      PlotLib.text(data.slice(0, 1), {
+        x: -1,
+        y: "name",
+        dx: dxValue,
+        dy: dyHeader,
+        text: d => "Value",
+        fontWeight: 500,
+        textAnchor: "end",
+      }),
+      PlotLib.text(data.slice(0, 1), {
+        x: 0,
+        y: "name",
+        dy: dyHeader,
+        fontWeight: 500,
+        text: d => `Shapley (sum: ${sum(data, d => +d.shapValue).toFixed(3)})`,
+      }),
+
+      // // text mark for y labels for flexibility
+      // PlotLib.text(data, {
+      //   x: -1,
+      //   y: "name",
+      //   textAnchor: "end",
+      //   dx: dxName,
+      //   // text: d => `${Number.isInteger(d.value) ? d.value : d.value.toFixed(3)} = ${d.name}`,
+      //   text: d => d.name,
+      // }),
+
+      // feature values
       PlotLib.text(data, {
         x: -1,
         y: "name",
+        text: "value",
+        dx: dxValue,
         textAnchor: "end",
-        dx: -5,
-        text: d => `${Number.isInteger(d.value) ? d.value : d.value.toFixed(3)} = ${d.name}`,
       }),
+
+      // bars
       PlotLib.barX(data, {
         x: "shapValue",
         y: "name",
         fill: d => (d.shapValue < 0 ? negColor : posColor),
       }),
+
+      // show nunbers in or next to bars
       PlotLib.text(
         data.filter(d => d.shapValue > textInBarCutoff),
-        { x: "shapValue", y: "name", text: "shapValue", textAnchor: "end", fill: "#fff", dx: -4 }
+        {
+          x: "shapValue",
+          y: "name",
+          text: "shapValue",
+          textAnchor: "end",
+          fill: "#fff",
+          dx: -4,
+          fontSize: 11,
+        }
       ),
       PlotLib.text(
         data.filter(d => d.shapValue < -textInBarCutoff),
-        { x: "shapValue", y: "name", text: "shapValue", textAnchor: "start", fill: "#fff", dx: 4 }
+        {
+          x: "shapValue",
+          y: "name",
+          text: "shapValue",
+          textAnchor: "start",
+          fill: "#fff",
+          dx: 4,
+          fontSize: 11,
+        }
       ),
       PlotLib.text(
         data.filter(d => d.shapValue > 0 && d.shapValue < textInBarCutoff),
-        { x: "shapValue", y: "name", text: "shapValue", textAnchor: "start", dx: 4 }
+        { x: "shapValue", y: "name", text: "shapValue", textAnchor: "start", dx: 4, fontSize: 11 }
       ),
       PlotLib.text(
         data.filter(d => d.shapValue < 0 && d.shapValue > -textInBarCutoff),
-        { x: "shapValue", y: "name", text: "shapValue", textAnchor: "end", dx: -4 }
+        { x: "shapValue", y: "name", text: "shapValue", textAnchor: "end", dx: -4, fontSize: 11 }
       ),
       // PlotLib.text(
       //   data.filter(d => d.shapValue === 0),
@@ -331,6 +391,22 @@ function getGroupResults(data) {
   return rows;
 }
 
+export const PRIORITISATION_COLORS = [
+  rgb("#a01813"),
+  rgb("#bc3a19"),
+  rgb("#d65a1f"),
+  rgb("#e08145"),
+  rgb("#e3a772"),
+  rgb("#e6ca9c"),
+  rgb("#eceada"),
+  rgb("#c5d2c1"),
+  rgb("#9ebaa8"),
+  rgb("#78a290"),
+  rgb("#528b78"),
+  rgb("#2f735f"),
+  rgb("#2e5943"),
+];
+
 function getTargetGroupFeatures(targetId, groupName) {
   const row = fakeData.find(d => d.geneId === targetId);
   return row.features.filter(feature => featureToGroup[feature.name] === groupName);
@@ -346,11 +422,10 @@ function getColorScale(groupResults) {
     }
   }
   Math.abs(min) > max ? (max = -min) : (min = -max);
-
-  // return scaleDiverging(t => interpolateRdBu(t * 0.6)).domain([min, max]);
+  // hacky - avoid extreme colors
+  min *= 1.3;
+  max *= 1.3;
   return scaleDiverging(interpolateRdBu).domain([min, 0, max]);
-  // return scaleDiverging([min, 0, max], interpolateRdBu).range([0.2, 0.8]);
-  // return scaleDiverging().domain([min, 0, max]).range([0.2, 0.8]);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
