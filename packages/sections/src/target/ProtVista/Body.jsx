0beff2501ac37c291842edde416a064c2640d3f6
Graham McNeill <graham.j.mcneill@gmail.com>
2025-02-27 11:01:24 +0000

    3dmol viewer

--- packages/sections/src/target/ProtVista/Body.jsx
+++ packages/sections/src/target/ProtVista/Body.jsx
@@ -1,10 +1,14 @@
 import { SectionItem, usePlatformApi, OtTable } from "ui";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { naLabel } from "../../constants";
 import Description from "./Description";
 import { definition } from ".";
 import { getUniprotIds } from "../../utils/global";
 import ProtVista from "./ProtVista";
+// import * as mol3d from "3dmol";
+import { createViewer } from "3dmol";
+
+// debugger;
 
 // import * as molstar from "";
 // import NightingaleVis from "./NightingaleVis";
@@ -58,6 +62,21 @@ function Body({ label: symbol, entity }) {
       },
       exportValue: false,
     },
+    {
+      id: "select",
+      label: "Structure",
+      renderCell: ({ id }) => {
+        return (
+          <Box
+            sx={{ color: "steelblue", "&:hover": { cursor: "pointer" } }}
+            onClick={() => setSelectedId(id)}
+          >
+            view
+          </Box>
+        );
+      },
+      exportValue: false,
+    },
     // TO ADD:
     //  - PDBe LINK
     //    - if only 1 link per row, could make the ID the link
@@ -81,7 +100,62 @@ function Body({ label: symbol, entity }) {
   }, [uniprotId]);
 
   // fetch selected structure
-  useEffect(() => {}, [selectedId]);
+  useEffect(() => {
+    async function fetchStructure(structureId) {
+      if (selectedId && viewerRef?.current) {
+        // console.log("STRUCTURE VIEWER EFFECT");
+        let config = { backgroundColor: "#f8f8f8", antialias: true };
+        let viewer = createViewer(viewerRef.current, config);
+
+        // viewer.setBackgroundColor("#f4f4f4");
+
+        // let pdbUri = "/path/to/your/pdb/files/1ycr.pdb";
+
+        // const pdbUri = "https://files.rcsb.org/download/1CRN.pdb";
+        const pdbUri = `https://files.rcsb.org/download/${selectedId}.pdb`;
+        // const pdbUri = "https://files.rcsb.org/download/1n26.pdb";
+        // const pdbUri = "https://files.rcsb.org/download/1CRN.pdb";
+        // const pdbUri = "https://www.ebi.ac.uk/pdbe/entry-files/download/4k33.bcif";
+        // const pdbUri = "https://alphafold.ebi.ac.uk/files/AF-A0A2K5QXN6-F1-model_v4.pdb";
+        // "https://alphafold.ebi.ac.uk/files/AF-Q9Y3C4-F1-model_v4.pdb";
+
+        const data = await (await fetch(pdbUri)).text();
+
+        // Define confidence color mapping (pLDDT stored in B-factor field)
+        var colorScheme = {
+          prop: "bfactor", // Use B-factor (pLDDT confidence score)
+          // gradient: "rwb", // Red (low) → White (medium) → Blue (high)
+          // min: 50, // pLDDT below 50 (low confidence)
+          // max: 100, // pLDDT above 90 (high confidence)
+        };
+
+        const colorAsSnake = function (atom) {
+          // debugger;
+          // console.log(atom.b);
+          if (atom.b > 90) return "blue";
+          if (atom.b > 70) return "skyblue";
+          if (atom.b > 50) return "yellow";
+          return "orange";
+          // return atom.b > 92 ? "lime" : "magenta";
+          // return atom.resi % 2 == 0 ? "white" : "green";
+        };
+
+        const v = viewer;
+        v.addModel(data, "pdb"); /* load data */
+        // v.setStyle({}, { cartoon: { color: "spectrum" } }); /* style all atoms */
+        // v.setStyle({}, { cartoon: { color: "spectrum" } }); /* style all atoms */
+        // Apply coloring by confidence
+        v.setStyle({ chain: "A" }, { cartoon: { colorfunc: colorAsSnake } });
+        // v.setStyle({ cartoon: { color: colorScheme } });
+        // v.setStyle({}, { cartoon: { color: "bfactor" } });
+        v.zoomTo(); /* set camera */
+        v.render(); /* render scene */
+        v.zoom(1.2, 1000); /* slight zoom */
+      }
+    }
+    fetchStructure();
+    // }, []); // SHOULD BE BELOW BUT HOOKS ERROR!
+  }, [selectedId]);
 
   return (
     <SectionItem
@@ -93,7 +167,13 @@ function Body({ label: symbol, entity }) {
       renderBody={() => {
         return (
           <>
-            <Box ref="viewerRef" width="800px" height="600px" />
+            <Box position="relative" display="flex" justifyContent="center" pb={2}>
+              <Box ref={viewerRef} position="relative" width="100%" height="400px">
+                <Box position="absolute" p={1} zIndex={100}>
+                  {selectedId}
+                </Box>
+              </Box>
+            </Box>
             <OtTable
               // dataDownloader
               showGlobalFilter={false}
