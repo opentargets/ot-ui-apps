d2732e37c920ddd96c74f4155f245df40268655d
Graham McNeill <graham.j.mcneill@gmail.com>
2025-02-28 10:37:12 +0000

    cleanup and use cif files

--- packages/sections/src/target/ProtVista/Body.jsx
+++ packages/sections/src/target/ProtVista/Body.jsx
@@ -5,29 +5,24 @@ import Description from "./Description";
 import { definition } from ".";
 import { getUniprotIds } from "../../utils/global";
 import ProtVista from "./ProtVista";
-// import * as mol3d from "3dmol";
 import { createViewer } from "3dmol";
-
-// import * as molstar from "";
 // import NightingaleVis from "./NightingaleVis";
 
 import PROTVISTA_SUMMARY_FRAGMENT from "./summaryQuery.gql";
 import { useState, useEffect, useRef } from "react";
 
 const experimentalResultsStem = "https://www.ebi.ac.uk/proteins/api/proteins/";
-const experimentalStructureStem = "https://files.rcsb.org/download/"; // !! EBI BETTER BUT OK FOR NOW
-const experimentalStructureSuffix = ".pdb";
+const experimentalStructureStem = "https://www.ebi.ac.uk/pdbe/entry-files/download/";
+const experimentalStructureSuffix = ".cif";
 const alphaFoldResultsStem = "https://alphafold.ebi.ac.uk/api/prediction/";
 const alphaFoldStructureStem = "https://alphafold.ebi.ac.uk/files/";
-const alphaFoldStructureSuffix = "-model_v4.pdb";
+const alphaFoldStructureSuffix = "-model_v4.cif";
 
 function Body({ label: symbol, entity }) {
   const [experimentalResults, setExperimentalResults] = useState(null);
   const [selectedId, setSelectedId] = useState(null);
 
   const viewerRef = useRef(null);
-
-  // console.log(experimentalResults);
 
   const request = usePlatformApi(PROTVISTA_SUMMARY_FRAGMENT);
   const uniprotId = request?.data ? getUniprotIds(request?.data?.proteinIds)?.[0] : null;
@@ -87,7 +82,6 @@ function Body({ label: symbol, entity }) {
     //  - PDBe LINK
     //    - if only 1 link per row, could make the ID the link
     //    - use identifiers.org
-    //  - AND BUTTON TO SELECT ROW
   ];
 
   // fetch experimental results
@@ -100,7 +94,6 @@ function Body({ label: symbol, entity }) {
         );
         if (response.error) throw response.error;
         if (response?.length > 0) {
-          console.log(response);
           results.unshift({
             id: response[0].entryId,
             type: "AlphaFold",
@@ -138,23 +131,12 @@ function Body({ label: symbol, entity }) {
   useEffect(() => {
     async function fetchStructure(structureId) {
       if (selectedId && viewerRef?.current) {
-        // console.log("STRUCTURE VIEWER EFFECT");
         let config = { backgroundColor: "#f8f8f8", antialias: true };
         let viewer = createViewer(viewerRef.current, config);
 
-        // viewer.setBackgroundColor("#f4f4f4");
-
-        // let pdbUri = "/path/to/your/pdb/files/1ycr.pdb";
-
-        // const pdbUri = "https://files.rcsb.org/download/1CRN.pdb";
         const pdbUri = selectedId.startsWith("AF")
           ? `${alphaFoldStructureStem}${selectedId}${alphaFoldStructureSuffix}`
-          : `${experimentalStructureStem}${selectedId}${experimentalStructureSuffix}`;
-        // const pdbUri = "https://files.rcsb.org/download/1n26.pdb";
-        // const pdbUri = "https://files.rcsb.org/download/1CRN.pdb";
-        // const pdbUri = "https://www.ebi.ac.uk/pdbe/entry-files/download/4k33.bcif";
-        // const pdbUri = "https://alphafold.ebi.ac.uk/files/AF-A0A2K5QXN6-F1-model_v4.pdb";
-        // "https://alphafold.ebi.ac.uk/files/AF-Q9Y3C4-F1-model_v4.pdb";
+          : `${experimentalStructureStem}${selectedId.toLowerCase()}${experimentalStructureSuffix}`;
 
         const data = await (await fetch(pdbUri)).text();
 
@@ -166,32 +148,30 @@ function Body({ label: symbol, entity }) {
           // max: 100, // pLDDT above 90 (high confidence)
         };
 
-        const colorAsSnake = function (atom) {
-          // debugger;
-          // console.log(atom.b);
-          if (atom.b > 90) return "blue";
+        const colorOnConfidence = function (atom) {
+          if (atom.b > 90) return "royalblue";
           if (atom.b > 70) return "skyblue";
           if (atom.b > 50) return "yellow";
           return "orange";
-          // return atom.b > 92 ? "lime" : "magenta";
-          // return atom.resi % 2 == 0 ? "white" : "green";
         };
 
         const v = viewer;
-        v.addModel(data, "pdb"); /* load data */
-        // v.setStyle({}, { cartoon: { color: "spectrum" } }); /* style all atoms */
-        // v.setStyle({}, { cartoon: { color: "spectrum" } }); /* style all atoms */
-        // Apply coloring by confidence
-        v.setStyle({ chain: "A" }, { cartoon: { colorfunc: colorAsSnake } });
-        // v.setStyle({ cartoon: { color: colorScheme } });
-        // v.setStyle({}, { cartoon: { color: "bfactor" } });
+        v.addModel(data, "cif"); /* load data */
+        v.setStyle(
+          {},
+          {
+            cartoon: selectedId.startsWith("AF")
+              ? { colorfunc: colorOnConfidence }
+              : { color: "spectrum" },
+          }
+        );
         v.zoomTo(); /* set camera */
         v.render(); /* render scene */
         v.zoom(1.2, 1000); /* slight zoom */
       }
     }
     fetchStructure();
-    // }, []); // SHOULD BE BELOW BUT HOOKS ERROR!
+    // RETURN CLEANUP FUNCTION IF APPROP
   }, [selectedId]);
 
   if (!request.data) return null; // BETTER WAY? - HANDLED BY CC'S CHANGE TO SECTION ITEM IF LOADING?
@@ -260,17 +240,13 @@ export default Body;
 
 NOTES:
 
-- list of structures:
-  - experimental: https://www.ebi.ac.uk/proteins/api/proteins/P35225   (the UniProt id)
-    - list is in dbReferences field, need to filter on type === "PDB"
-  - alphaFold: https://alphafold.ebi.ac.uk/api/prediction/P35225
-
-- Then to get the actual structure file: https://www.ebi.ac.uk/pdbe/entry-files/download/{identifier}.bcif
-  - for the alphaFold, use -model_v4 at the end (or see what latest model is)
-
 - currently will stick with the uniprot_swissprot test for whether to include widget
   - this will always have an alphaFold prediction - and poss no experimental
 
 - should include some visual controls - e.g. cartoon, ball-stick
+
+- how indicate currently selected row - usint selectedId === id in a recderCell callback does not work
+
+- if use paginated table. what if selected row not shown - what does the viewer show?
 
 */
