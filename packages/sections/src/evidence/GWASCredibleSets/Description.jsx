5105bf6e0fd366615fa9e878fba68ed18133d2c1
Carlos Cruz <me@carcruzcast.com>
2025-03-13 11:40:00 +0000

    feat: show all

--- packages/sections/src/evidence/GWASCredibleSets/Description.jsx
+++ packages/sections/src/evidence/GWASCredibleSets/Description.jsx
@@ -3,7 +3,7 @@ import { Link } from "ui";
 function Description({ symbol, name }) {
   return (
     <>
-      95% GWAS credible sets prioritisating <strong>{symbol}</strong> as likely causal gene for{" "}
+      95% GWAS credible sets prioritising <strong>{symbol}</strong> as likely causal gene for{" "}
       <strong>{name}</strong>. Source:{" "}
       <Link to="https://home.opentargets.org/merged-product-documentation" external>
         Open Targets
