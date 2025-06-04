576aedfe5ce12f3d9824dfc0c4f14a68eff258ae
Javier Ferrer <jferrer@ebi.ac.uk>
2025-06-04 16:00:15 +0100

    [Platform] Refactor disease ontology widget to use API

--- packages/sections/src/disease/Ontology/Body.jsx
+++ packages/sections/src/disease/Ontology/Body.jsx
@@ -1,63 +1,31 @@
-import { useState, useEffect } from "react";
-import { SectionItem, useConfigContext } from "ui";
+import { SectionItem } from "ui";
 
 import Description from "./Description";
+import ONTOLOGY_QUERY from "./OntologyQuery.gql";
+
+import { useQuery } from "@apollo/client";
+import { definition } from ".";
 import OntologySubgraph from "./OntologySubgraph";
 
-import { definition } from "./index";
-
-function Body({ id: efoId, label: name, entity }) {
-  const { config } = useConfigContext();
-
-  const [efoNodes, setEfoNodes] = useState({
-    allNodes: null,
-    filteredNodes: null,
+function Body({ id: efoId, label, entity }) {
+  const request = useQuery(ONTOLOGY_QUERY, {
+    variables: { efoId },
   });
-
-  function requestEfoNodes() {
-    fetch(config.efoURL)
-      .then(res => res.text())
-      .then(lines => {
-        const nodes = lines.trim().split("\n").map(JSON.parse);
-        const idToDisease = nodes.reduce((acc, disease) => {
-          acc[disease.id] = disease;
-          return acc;
-        }, {});
-        setEfoNodes({ allNodes: nodes, filteredNodes: idToDisease });
-      });
-  }
-
-  useEffect(() => {
-    let isCurrent = true;
-    if (isCurrent) requestEfoNodes();
-
-    return () => {
-      isCurrent = false;
-    };
-  }, []);
 
   return (
     <SectionItem
       definition={definition}
       entity={entity}
-      request={{
-        loading: !efoNodes.filteredNodes,
-        data: {
-          [entity]: { efoNodes: efoNodes.allNodes },
-        },
-      }}
+      request={request}
       showContentLoading={true}
-      renderDescription={() => <Description name={name} />}
-      renderBody={() => {
-        return (
-          <OntologySubgraph
-            efoId={efoId}
-            efo={efoNodes.allNodes}
-            name={name}
-            idToDisease={efoNodes.filteredNodes}
-          />
-        );
-      }}
+      renderDescription={() => <Description name={label} />}
+      renderBody={() =>
+        <OntologySubgraph
+          name={label}
+          data={request.data?.disease}
+        />
+      }
+      chipText={label}
     />
   );
 }
