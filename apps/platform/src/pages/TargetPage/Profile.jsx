1dae207c2038390e30bc6ad035f9a4de5cfba119
Graham McNeill <graham.j.mcneill@gmail.com>
2025-06-25 09:46:03 +0100

    show all target profile widgets except overlapping variants

--- apps/platform/src/pages/TargetPage/Profile.jsx
+++ apps/platform/src/pages/TargetPage/Profile.jsx
@@ -26,7 +26,7 @@ import MousePhenotypesSummary from "sections/src/target/MousePhenotypes/Summary"
 import ComparativeGenomicsSummary from "sections/src/target/ComparativeGenomics/Summary";
 import SubcellularLocationSummary from "sections/src/target/SubcellularLocation/Summary";
 import BibliographySummary from "sections/src/target/Bibliography/Summary";
-import OverlappingVariantsSummary from "sections/src/target/OverlappingVariants/Summary";
+// import OverlappingVariantsSummary from "sections/src/target/OverlappingVariants/Summary";
 
 import ProfileHeader from "./ProfileHeader";
 
@@ -53,9 +53,9 @@ const SubcellularLocationSection = lazy(() =>
   import("sections/src/target/SubcellularLocation/Body")
 );
 const BibliographySection = lazy(() => import("sections/src/target/Bibliography/Body"));
-const OverlappingVariantsSection = lazy(() =>
-  import("sections/src/target/OverlappingVariants/Body")
-);
+// const OverlappingVariantsSection = lazy(() =>
+//   import("sections/src/target/OverlappingVariants/Body")
+// );
 
 const summaries = [
   KnownDrugsSummary,
@@ -75,7 +75,7 @@ const summaries = [
   ComparativeGenomicsSummary,
   SubcellularLocationSummary,
   BibliographySummary,
-  OverlappingVariantsSummary,
+  // OverlappingVariantsSummary,
 ];
 
 const TARGET = "target";
@@ -107,9 +107,9 @@ function Profile({ ensgId, symbol }) {
         <SubcellularLocationSummary />
         <GeneOntologySummary />
         <GeneticConstraintSummary />
-        <PrivateWrapper>
+        {/* <PrivateWrapper>
           <OverlappingVariantsSummary />
-        </PrivateWrapper>
+        </PrivateWrapper> */}
         <MolecularStructureSummary />
         <MolecularInteractionsSummary />
         <PathwaysSummary />
@@ -120,7 +120,7 @@ function Profile({ ensgId, symbol }) {
       </SummaryContainer>
 
       <SectionContainer>
-        {/* <Suspense fallback={<SectionLoader />}>
+        <Suspense fallback={<SectionLoader />}>
           <KnownDrugsSection id={ensgId} label={symbol} entity={TARGET} />
         </Suspense>
         <Suspense fallback={<SectionLoader />}>
@@ -149,16 +149,16 @@ function Profile({ ensgId, symbol }) {
         </Suspense>
         <Suspense fallback={<SectionLoader />}>
           <GeneticConstraintSection id={ensgId} label={symbol} entity={TARGET} />
-        </Suspense> */}
-        <PrivateWrapper>
+        </Suspense>
+        {/* <PrivateWrapper>
           <Suspense fallback={<SectionLoader />}>
             <OverlappingVariantsSection id={ensgId} label={symbol} entity={TARGET} />
           </Suspense>
-        </PrivateWrapper>
+        </PrivateWrapper> */}
         <Suspense fallback={<SectionLoader />}>
           <MolecularStructureSection id={ensgId} label={symbol} entity={TARGET} />
         </Suspense>
-        {/* <Suspense fallback={<SectionLoader />}>
+        <Suspense fallback={<SectionLoader />}>
           <MolecularInteractionsSection id={ensgId} label={symbol} entity={TARGET} />
         </Suspense>
         <Suspense fallback={<SectionLoader />}>
@@ -175,7 +175,7 @@ function Profile({ ensgId, symbol }) {
         </Suspense>
         <Suspense fallback={<SectionLoader />}>
           <BibliographySection id={ensgId} label={symbol} entity={TARGET} />
-        </Suspense> */}
+        </Suspense>
       </SectionContainer>
     </PlatformApiProvider>
   );
