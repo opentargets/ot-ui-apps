b256223c9f90c728858382d55dd3fa6b05040ce2
Carlos Cruz <me@carcruzcast.com>
2025-07-08 16:52:21 +0100

    feat: credible set sections

--- packages/sections/src/index.tsx
+++ packages/sections/src/index.tsx
@@ -3,6 +3,7 @@ export * as Drug from "./drug";
 export * as Disease from "./disease";
 export * as Target from "./target";
 export * as Variant from "./variant";
+export * as Study from "./study";
 
 // Export types
 export * from "./types/widget";
