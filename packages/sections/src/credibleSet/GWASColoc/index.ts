0f19c4610c0c4ae9aacf8f66646362616b1a82b9
Carlos Cruz <me@carcruzcast.com>
2025-07-08 16:52:31 +0100

    feat: credible set sections

--- packages/sections/src/credibleSet/GWASColoc/index.ts
+++ packages/sections/src/credibleSet/GWASColoc/index.ts
@@ -1,7 +1,12 @@
-const id = "gwas_coloc";
+import { lazy } from "react";
+import { CredibleSet } from "@ot/constants";
+
 export const definition = {
-  id,
+  id: "gwas_coloc",
   name: "GWAS Colocalisation",
   shortName: "GC",
-  hasData: data => data?.colocalisation?.count > 0,
+  hasData: (data: CredibleSet) => (data.colocalisation?.count || 0) > 0,
 };
+
+export { default as Summary } from "./Summary";
+export const getBodyComponent = () => lazy(() => import("./Body"));
