9cebb62eb8356115e2b65297e04d5271912a2544
David Ochoa <ochoa@ebi.ac.uk>
2025-09-05 17:23:34 +0100

    fix: GWAS studies as GWAS when possible

--- packages/sections/src/credibleSet/GWASColoc/Description.tsx
+++ packages/sections/src/credibleSet/GWASColoc/Description.tsx
@@ -3,7 +3,7 @@ import { Link } from "ui";
 function Description() {
   return (
     <>
-      Colocalisation metrics for overlapping credible sets from GWAS studies. Source:{" "}
+      Colocalisation metrics for overlapping GWAS credible sets. Source:{" "}
       <Link to="https://platform-docs.opentargets.org/credible-set#colocalisation" external>
         Open Targets
       </Link>
