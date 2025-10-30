f40ff921e3b2233a1677f8dcbb3bf5dbb38fe090
Carlos Cruz <me@carcruzcast.com>
2025-10-30 13:20:48 +0000

    [AppConfig]: biome config fix (#836)
    * fix: biome version and edifor config
    
    * fix: set static version
    
    * [ot-config]: lint on (#821)
    
    * [ot-utils]: lint on (#822)
    
    * [ot-constants]: lint on (#826)
    
    ---------
    
    Co-authored-by: Graham McNeill <gjmcn@users.noreply.github.com>

--- packages/ot-utils/src/alphaFold.ts
+++ packages/ot-utils/src/alphaFold.ts
@@ -1,7 +1,7 @@
 import { csvParse, mean } from "d3";
 import { safeFetch } from "./fetch";
-import { alphaFoldPathogenicityUrl, alphaFoldDomainsUrl } from "./urls";
-  
+import { alphaFoldDomainsUrl, alphaFoldPathogenicityUrl } from "./urls";
+
 // returns map of pathogenicity scores grouped by residue index (number). Each
 // group is an array of objects giving pathogencity scores for alternate amino
 // acids:
@@ -9,21 +9,20 @@ import { alphaFoldPathogenicityUrl, alphaFoldDomainsUrl } from "./urls";
 //    protein_variant (string): e.g. "M1E" - current AA, residue index, alternate AA
 //    am_pathogenicity (string): score
 //    am_class (string)
-//  } 
+//  }
 export async function fetchPathogenicityScores(uniprotId: string) {
-  const [scoresCSV, error] =
-    await safeFetch(alphaFoldPathogenicityUrl(uniprotId), "text");
+  const [scoresCSV, error] = await safeFetch(alphaFoldPathogenicityUrl(uniprotId), "text");
   if (error) return [undefined, error];
   const scores = Map.groupBy(
-    csvParse(scoresCSV),  // string -> array of objects
-    row => Number(row.protein_variant.match(/\d+/)[0])  
+    csvParse(scoresCSV), // string -> array of objects
+    (row) => Number(row.protein_variant.match(/\d+/)[0])
   );
   return [scores, undefined];
-};
+}
 
 export async function fetchDomains(uniprotId: string) {
   return safeFetch(alphaFoldDomainsUrl(uniprotId), "json");
-};
+}
 
 // returns map:
 // - key (number): residue index
@@ -31,7 +30,10 @@ export async function fetchDomains(uniprotId: string) {
 export function meanPathogenicityScores(pathogenicityScores) {
   const meanScores = new Map();
   for (const [resi, group] of pathogenicityScores) {
-    meanScores.set(resi, mean(group, d => Number(d.am_pathogenicity)));
+    meanScores.set(
+      resi,
+      mean(group, (d) => Number(d.am_pathogenicity))
+    );
   }
   return meanScores;
 }
@@ -40,13 +42,12 @@ export function meanPathogenicityScores(pathogenicityScores) {
 export function pickPathogenicityScore(pathogenicityScores, resi, alternateAminoAcid) {
   const row = pathogenicityScores
     .get(Number(resi))
-    .find(row => row.protein_variant.at(-1) === alternateAminoAcid);
+    .find((row) => row.protein_variant.at(-1) === alternateAminoAcid);
   return row ? Number(row.am_pathogenicity) : undefined;
 }
 
 export function processDomains(domains) {
-  const uniqueDescriptions =
-    [...new Set(domains.map(domain => domain.description))];
+  const uniqueDescriptions = [...new Set(domains.map((domain) => domain.description))];
   const descriptionToIndex = {};
   for (const [index, description] of uniqueDescriptions.entries()) {
     descriptionToIndex[description] = index;
