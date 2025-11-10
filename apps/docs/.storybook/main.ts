ea4ae0e210e96125bbc71eb1d318f27f37426d33
Carlos Cruz <me@carcruzcast.com>
2025-11-10 09:58:57 +0000

    [UI]: Upgrade Storybook and Migrate Components to TypeScript
    - Updated Storybook dependencies to version 10.0.6 across the project.
    - Migrated Storybook configuration files to ESM format.
    - Added new components: Chip and Link with corresponding stories.
    - Removed deprecated button and header components along with their styles.
    - Introduced a new preview head file for global configuration.
    - Updated React and React DOM dependencies to version 18.2.0.
    - Cleaned up unused CSS files and adjusted component imports.

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,23 +1,40 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { createRequire } from "node:module";
+import type { StorybookConfig } from "@storybook/react-vite";
+import { dirname, join } from "path";
+import { mergeConfig } from "vite";
 
-import { join, dirname } from "path"
+const require = createRequire(import.meta.url);
+const gql = require("vite-plugin-simple-gql").default;
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../../../packages/sections/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../../../packages/sections/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [],
-  "framework": {
-    "name": getAbsolutePath('@storybook/react-vite'),
-    "options": {}
-  }
+
+  addons: [],
+
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [gql()],
+      esbuild: {
+        jsxInject: `import React from 'react'`,
+      },
+    });
+  },
 };
 export default config;
