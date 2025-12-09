1146127e61e9868827ca2c2be8dc4f1ec482b2d3
david oluwasusi <daviddamilola20@gmail.com>
2025-12-09 16:25:14 +0000

    feat(homepage:e2e): add test suite for homepage actions

--- packages/platform-test/e2e/pages/homepage.spec.ts
+++ packages/platform-test/e2e/pages/homepage.spec.ts
@@ -1,7 +1,93 @@
-import { test, expect } from "@playwright/test";
+import { expect, test, Locator } from "@playwright/test";
 
-test("Validate page title", async ({ page, baseURL }) => {
-  await page.goto(baseURL!);
-  const title = await page.title();
-  await expect(title).toBe("Open Targets Platform");
+// https://github.com/microsoft/playwright/issues/36395
+async function fillPolling(locator: Locator, value: string) {
+  await expect
+    .poll(async () => {
+      await locator.fill(value);
+      return await locator.inputValue();
+    })
+    .toEqual(value);
+}
+
+test.describe("Home page actions", () => {
+  test("Validate page title", async ({ page, baseURL }) => {
+    await page.goto(baseURL!);
+    const title = await page.title();
+    await expect(title).toBe("Open Targets Platform");
+  });
+
+  test("Search page is functional, when user visits the homepage", async ({ page, baseURL }) => {
+    await page.goto(baseURL!);
+    await page.getByTestId("global-search-input-trigger").click();
+
+    // Verify that the global search input is rendered
+    const searchInput = page.getByTestId("global-search-input");
+    await expect(searchInput).toBeVisible();
+
+    // Fill the search input with MI
+    await fillPolling(searchInput, "myocardial infarction");
+    await expect(searchInput).toHaveValue("myocardial infarction");
+
+    // Submit the search form
+    await searchInput.press("Enter");
+
+    // Verify that the URL contains the search query
+    await expect(page).toHaveURL(/.*search.*/);
+  });
+
+  test("Search suggestions is functional and displayed correctly", async ({ page, baseURL }) => {
+    await page.goto(baseURL!);
+    await page.getByTestId("global-search-input-trigger").click();
+
+    // Verify that the global search input is rendered
+    const searchInput = await page.getByTestId("global-search-input");
+    await expect(searchInput).toBeVisible();
+
+    // Fill the search input with MI
+    await fillPolling(searchInput, "myocardial infarction");
+
+    const topHit = await page.getByTestId("top-hit-list-item-topHit"); 
+    await expect(topHit).toBeVisible();
+
+     const targets = await page.getByTestId("top-hit-list-item-targets");
+    await expect(targets).toBeVisible();
+
+    const gwas_studies = await page.getByTestId("top-hit-list-item-studies");
+    await expect(gwas_studies).toBeVisible();
+
+    const diseases = await page.getByTestId("top-hit-list-item-diseases");
+    await expect(diseases).toBeVisible();
+
+    const drugs = await page.getByTestId("top-hit-list-item-drugs");
+    await expect(drugs).toBeVisible();
+
+    await page.getByTestId("top-hit-list-result-item").first().click();
+
+    // Verify that the URL contains the disease page
+    await expect(page).toHaveURL(/.*disease\.*/);
+    
+  });
+
+    test("Can go to the disease page from the search suggestions", async ({ page, baseURL }) => {
+      await page.goto(baseURL!);
+      await page.getByTestId("global-search-input-trigger").click();
+
+      // Verify that the global search input is rendered
+      const searchInput = await page.getByTestId("global-search-input");
+      await expect(searchInput).toBeVisible();
+
+      // Fill the search input with MI
+      await fillPolling(searchInput, "myocardial infarction");
+      // Filter by only disease
+      await page.getByTestId("entity-filter-disease").first().click();
+
+      const diseases = await page.getByTestId("top-hit-list-item-diseases");
+      await expect(diseases).toBeVisible();
+
+      await page.getByTestId("top-hit-list-result-item").first().click();
+
+      // Verify that the URL contains the disease page
+      await expect(page).toHaveURL(/.*disease\.*/);
+    });
 });
--- packages/platform-test/readme.md
+++ packages/platform-test/readme.md
@@ -7,3 +7,4 @@ yarn playwright install
 - https://snyk.io/es/blog/how-to-add-playwright-tests-pr-ci-github-actions/
 - https://dev.to/thiernope/trigger-netlify-deploys-using-github-workflow-cicd-lcm
 - https://github.com/nwtgck/actions-netlify/issues/1220
+- https://testomat.io/blog/grouping-playwright-tests-for-improved-framework-efficiency/
