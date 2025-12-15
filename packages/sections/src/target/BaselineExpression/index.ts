a4761c2c3000768e365b439392246e9704a1feda
Graham McNeill <graham.j.mcneill@gmail.com>
2025-12-15 12:08:41 +0000

    update hasData and simplify fragment query

--- packages/sections/src/target/BaselineExpression/index.ts
+++ packages/sections/src/target/BaselineExpression/index.ts
@@ -6,14 +6,10 @@ export const definition = {
   name: "Baseline Expression",
   shortName: "BE",
   hasData: (data: TargetData) => {
-    return true; // !! NEEDS UPDATED !!
-    // const hasRNA = data.expressions?.some(d => (d.rna?.level || 0) >= 0) || false;
-    // const hasProtein = data.expressions?.some(d => (d.protein?.level || 0) >= 0) || false;
-    // // TODO:
-    // // the check for gtex data should be remove if/when
-    // // we stop checking for data on switching tab (see comment in GtexTab)
-    // const hasGtex = data.expressions?.some(d => d.tissueSiteDetailId) || false;
-    // return hasRNA || hasProtein || hasGtex;
+    return (
+      (data.baselineExpression?.count || 0) > 0 || // main widget tab
+      (data.expressions?.length || 0) > 0 // GTEx tab
+    );
   },
 };
 
