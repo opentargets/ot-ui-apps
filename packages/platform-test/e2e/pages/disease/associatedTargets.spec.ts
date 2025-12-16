c4c32c6630cc47fe30f2f904d17c5302f1b372ca
david oluwasusi <daviddamilola20@gmail.com>
2025-12-16 10:25:05 +0000

    restructure object model for Evidence section

--- packages/platform-test/e2e/pages/disease/associatedTargets.spec.ts
+++ packages/platform-test/e2e/pages/disease/associatedTargets.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 import { AotfActions } from "../../../POM/objects/widgets/AOTF/aotfActions";
 import { AotfTable } from "../../../POM/objects/widgets/AOTF/aotfTable";
-import { EvidenceSection } from "../../../POM/objects/widgets/EvidenceSection/evidenceSection";
+import { EvidenceSection } from "../../../POM/objects/components/EvidenceSection/evidenceSection";
 import { DiseasePage } from "../../../POM/page/disease/disease";
 
 const DISEASE_EFO_ID = "EFO_0000612";
