53c62c395b701f04dcf7b34cabccde96796ec604
david oluwasusi <daviddamilola20@gmail.com>
2026-02-04 09:29:29 +0000

    feat(aotf): add interactors and tests for aotf target interactors

--- packages/platform-test/e2e/pages/disease/associatedTargetsPrioritization.spec.ts
+++ packages/platform-test/e2e/pages/disease/associatedTargetsPrioritization.spec.ts
@@ -2,7 +2,7 @@ import { test } from "../../../fixtures";
 import { AotfActions } from "../../../POM/objects/widgets/AOTF/aotfActions";
 import { AotfTable } from "../../../POM/objects/widgets/AOTF/aotfTable";
 
-test.describe("Disease Page - AOTF Prioritization", () => {
+test.describe("Disease Page - AOTF Prioritization", { tag: "@smoke" }, () => {
   test.beforeEach(async ({ page, baseURL, testConfig }) => {
     await page.goto(`${baseURL}/disease/${testConfig.disease.primary}/associations`);
   });
