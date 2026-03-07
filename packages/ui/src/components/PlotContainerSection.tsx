e85fa1c6f91847b1a7ca3c4270d7252521058005
raskolnikov-rodion <22417165+raskolnikov-rodion@users.noreply.github.com>
2026-03-07 10:31:41 +0000

    [UI]: migrate PlotContainerSection component to TypeScript

--- packages/ui/src/components/PlotContainerSection.tsx
+++ packages/ui/src/components/PlotContainerSection.tsx
@@ -1,13 +1,19 @@
+import { Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import { ReactNode } from "react";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme: Theme) => ({
   plotContainerSection: {
     padding: "4px 0",
     borderBottom: `1px solid ${theme.palette.grey[300]}`,
   },
 }));
 
-function PlotContainerSection({ children }) {
+type PlotContainerSectionProps = {
+  children: ReactNode;
+};
+
+function PlotContainerSection({ children }: PlotContainerSectionProps): ReactNode {
   const classes = useStyles();
   return <div className={classes.plotContainerSection}>{children}</div>;
 }
