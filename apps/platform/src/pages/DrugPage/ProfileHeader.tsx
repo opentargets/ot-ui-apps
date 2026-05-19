1e71600d4381a4b07cb5efeb9e80a105b190939b
Graham McNeill <graham.j.mcneill@gmail.com>
2026-05-19 16:13:05 +0100

    reduce diagram size and add zoom modal

--- apps/platform/src/pages/DrugPage/ProfileHeader.tsx
+++ apps/platform/src/pages/DrugPage/ProfileHeader.tsx
@@ -6,18 +6,26 @@ import {
   Field,
   ProfileHeader as BaseProfileHeader,
 } from "ui";
-import { Paper } from "@mui/material";
+import { Button, Paper, Modal } from "@mui/material";
 import { Fragment, useState } from "react";
 import { clinicalStageCategories } from "@ot/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlassPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {grey} from "@mui/material/colors";
 
 import DRUG_PROFILE_HEADER_FRAGMENT from "./ProfileHeader.gql";
 
 function ProfileHeader({ chemblId }: { chemblId: string }) {
   const { loading, error, data } = usePlatformApi();
   const [imgState, setImgState] = useState<'loading' | 'loaded' | 'error'>('loading');
+  const [isOpen, setIsOpen] = useState(false);
 
   // TODO: Errors!
   if (error) return null;
+
+  const toggleModal = (): void => {
+    setIsOpen(!isOpen);
+  };
 
   const {
     description,
@@ -62,15 +70,30 @@ function ProfileHeader({ chemblId }: { chemblId: string }) {
         </ProfileChipList>
       </>
       <Paper
+        component={Button}
         variant="outlined"
         elevation={0}
         sx={{
-          width: '340px',
-          height: '340px',
+          position: 'relative',
+          width: '240px',
+          height: '240px',
           marginLeft: 'auto',
           visibility: imgState === 'loaded' ? 'visible' : 'hidden',
           display: imgState === 'error' ? 'none' : 'block',
+          cursor: "zoom-in",
+          bgcolor: "#fff",
+          "&:hover": {
+            bgcolor: "#fff",
+            borderColor: grey[400],
+          },
+          "& .magnify-icon": {
+            color: grey[400],
+          },
+          "&:hover .magnify-icon": {
+            color: grey[700],
+          },
         }}
+        onClick={toggleModal}
       >
         <img 
           src={`https://www.ebi.ac.uk/chembl/api/data/image/${chemblId}.svg`}
@@ -78,7 +101,58 @@ function ProfileHeader({ chemblId }: { chemblId: string }) {
           onLoad={() => setImgState('loaded')}
           onError={() => setImgState('error')}
         />
+          <FontAwesomeIcon
+            icon={faMagnifyingGlassPlus}
+            className="magnify-icon"
+            style={{ position: 'absolute', bottom: 8, right: 8 }}
+          />
       </Paper>
+
+      <Modal
+        open={isOpen}
+        onClose={toggleModal}
+        keepMounted
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Paper
+          variant="outlined"
+          elevation={0}
+          sx={{
+            position: 'relative',
+            width: '440px',
+            height: '440px',
+          }}
+        >
+          <img 
+            src={`https://www.ebi.ac.uk/chembl/api/data/image/${chemblId}.svg`}
+            style={{width: "100%", height: "100%" }}
+          />
+          <Button
+            sx={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              zIndex: 1,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              bgcolor: "transparent",
+              border: "none",
+              p: 0,
+              "&:hover": {
+                bgcolor: grey[200],
+              },
+            }}
+            onClick={toggleModal} 
+          >
+            <FontAwesomeIcon icon={faXmark} />
+          </Button>
+        </Paper>
+      </Modal>
     </BaseProfileHeader>
   );
 }
